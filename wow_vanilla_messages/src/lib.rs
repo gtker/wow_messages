32b0501d6f46d8df9ce4dca6e69223c24addbe67
Gtker <git@gtker.com>
2022-04-30 20:42:11 +0200

    world: Add async *MessageWrites

--- wow_vanilla_messages/src/lib.rs
+++ wow_vanilla_messages/src/lib.rs
@@ -28,10 +28,15 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 pub const DEFAULT_PORT: u16 = 8085;
 
+#[cfg_attr(any(feature = "async_tokio", feature = "async_std"), async_trait)]
 pub trait ServerMessageWrite: MessageBody {
+    const OPCODE_LENGTH: u16 = 2;
+
     fn write_unencrypted_server<W: Write>(&self, w: &mut W) -> Result<(), std::io::Error> {
-        // size: u16_be, and opcode: u16
-        crate::util::write_u16_be(w, self.size_without_size_or_opcode_fields() + 2)?;
+        crate::util::write_u16_be(
+            w,
+            self.size_without_size_or_opcode_fields() + Self::OPCODE_LENGTH,
+        )?;
         crate::util::write_u16_le(w, <Self as MessageBody>::OPCODE)?;
 
         self.write_body(w)?;
@@ -43,22 +48,90 @@ pub trait ServerMessageWrite: MessageBody {
         w: &mut W,
         e: &mut E,
     ) -> Result<(), std::io::Error> {
-        // size: u16_be, and opcode: u16
         e.write_encrypted_server_header(
             w,
-            self.size_without_size_or_opcode_fields() + 2,
+            self.size_without_size_or_opcode_fields() + Self::OPCODE_LENGTH,
             <Self as MessageBody>::OPCODE,
         )?;
 
         self.write_body(w)?;
         Ok(())
     }
+
+    #[cfg(feature = "async_tokio")]
+    async fn tokio_write_unencrypted_server<W: AsyncWriteExt + Unpin + Send>(
+        &self,
+        w: &mut W,
+    ) -> Result<(), std::io::Error> {
+        crate::util::tokio_write_u16_be(
+            w,
+            self.size_without_size_or_opcode_fields() + Self::OPCODE_LENGTH,
+        )
+        .await?;
+        crate::util::tokio_write_u16_le(w, <Self as MessageBody>::OPCODE).await?;
+
+        self.tokio_write_body(w).await?;
+        Ok(())
+    }
+
+    #[cfg(feature = "async_tokio")]
+    async fn tokio_write_encrypted_server<W: AsyncWriteExt + Unpin + Send, E: Encrypter + Send>(
+        &self,
+        w: &mut W,
+        e: &mut E,
+    ) -> Result<(), std::io::Error> {
+        let header = e.encrypt_server_header(
+            self.size_without_size_or_opcode_fields() + Self::OPCODE_LENGTH,
+            <Self as MessageBody>::OPCODE,
+        );
+        w.write_all(&header);
+
+        self.tokio_write_body(w).await?;
+        Ok(())
+    }
+
+    #[cfg(feature = "async_std")]
+    async fn astd_write_unencrypted_server<W: WriteExt + Unpin + Send>(
+        &self,
+        w: &mut W,
+    ) -> Result<(), std::io::Error> {
+        crate::util::astd_write_u16_be(
+            w,
+            self.size_without_size_or_opcode_fields() + Self::OPCODE_LENGTH,
+        )
+        .await?;
+        crate::util::astd_write_u16_le(w, <Self as MessageBody>::OPCODE).await?;
+
+        self.astd_write_body(w).await?;
+        Ok(())
+    }
+
+    #[cfg(feature = "async_std")]
+    async fn astd_write_encrypted_server<W: WriteExt + Unpin + Send, E: Encrypter + Send>(
+        &self,
+        w: &mut W,
+        e: &mut E,
+    ) -> Result<(), std::io::Error> {
+        let header = e.encrypt_server_header(
+            self.size_without_size_or_opcode_fields() + Self::OPCODE_LENGTH,
+            <Self as MessageBody>::OPCODE,
+        );
+        w.write_all(&header).await;
+
+        self.astd_write_body(w).await?;
+        Ok(())
+    }
 }
 
+#[cfg_attr(any(feature = "async_tokio", feature = "async_std"), async_trait)]
 pub trait ClientMessageWrite: MessageBody {
+    const OPCODE_LENGTH: u16 = 4;
+
     fn write_unencrypted_client<W: Write>(&self, w: &mut W) -> Result<(), std::io::Error> {
-        // size: u16_be, and opcode: u32
-        crate::util::write_u16_be(w, (self.size_without_size_or_opcode_fields() + 4))?;
+        crate::util::write_u16_be(
+            w,
+            (self.size_without_size_or_opcode_fields() + Self::OPCODE_LENGTH),
+        )?;
         crate::util::write_u32_le(w, <Self as MessageBody>::OPCODE as u32)?;
 
         self.write_body(w)?;
@@ -70,14 +143,73 @@ pub trait ClientMessageWrite: MessageBody {
         w: &mut W,
         e: &mut E,
     ) -> Result<(), std::io::Error> {
-        // size: u16_be, and opcode: u32
         e.write_encrypted_client_header(
             w,
-            (self.size_without_size_or_opcode_fields() + 4),
+            (self.size_without_size_or_opcode_fields() + Self::OPCODE_LENGTH),
             <Self as MessageBody>::OPCODE as u32,
         )?;
 
         self.write_body(w)?;
+        Ok(())
+    }
+
+    #[cfg(feature = "async_tokio")]
+    async fn tokio_write_unencrypted_client<W: AsyncWriteExt + Unpin + Send>(
+        &self,
+        w: &mut W,
+    ) -> Result<(), std::io::Error> {
+        crate::util::tokio_write_u16_be(
+            w,
+            self.size_without_size_or_opcode_fields() + Self::OPCODE_LENGTH,
+        )
+        .await?;
+        crate::util::tokio_write_u32_le(w, <Self as MessageBody>::OPCODE as u32).await?;
+
+        self.tokio_write_body(w).await?;
+        Ok(())
+    }
+
+    #[cfg(feature = "async_tokio")]
+    async fn tokio_write_encrypted_client<W: AsyncWriteExt + Unpin + Send, E: Encrypter + Send>(
+        &self,
+        w: &mut W,
+        e: &mut E,
+    ) -> Result<(), std::io::Error> {
+        let header = e.encrypt_client_header(
+            self.size_without_size_or_opcode_fields() + 4,
+            <Self as MessageBody>::OPCODE as u32,
+        );
+        w.write_all(&header);
+
+        self.tokio_write_body(w).await?;
+        Ok(())
+    }
+
+    #[cfg(feature = "async_std")]
+    async fn astd_write_unencrypted_client<W: WriteExt + Unpin + Send>(
+        &self,
+        w: &mut W,
+    ) -> Result<(), std::io::Error> {
+        crate::util::astd_write_u16_be(w, self.size_without_size_or_opcode_fields() + 4).await?;
+        crate::util::astd_write_u32_le(w, <Self as MessageBody>::OPCODE as u32).await?;
+
+        self.astd_write_body(w).await?;
+        Ok(())
+    }
+
+    #[cfg(feature = "async_std")]
+    async fn astd_write_encrypted_client<W: WriteExt + Unpin + Send, E: Encrypter + Send>(
+        &self,
+        w: &mut W,
+        e: &mut E,
+    ) -> Result<(), std::io::Error> {
+        let header = e.encrypt_client_header(
+            self.size_without_size_or_opcode_fields() + 4,
+            <Self as MessageBody>::OPCODE as u32,
+        );
+        w.write_all(&header).await;
+
+        self.astd_write_body(w).await?;
         Ok(())
     }
 }
