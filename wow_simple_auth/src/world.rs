6d2dd62458681ed859e7915097e7d79c6c6f6b61
Gtker <git@gtker.com>
2022-05-26 13:40:05 +0200

    simple_auth: Add almost working login to world

--- wow_simple_auth/src/world.rs
+++ wow_simple_auth/src/world.rs
@@ -6,11 +6,12 @@ use tokio::time::sleep;
 use wow_srp::header_crypto::ProofSeed;
 use wow_srp::normalized_string::NormalizedString;
 use wow_srp::server::SrpServer;
-use wow_vanilla_messages::helper::{
-    tokio_read_expect_client_message, tokio_read_expect_client_message_encryption,
-};
+use wow_vanilla_messages::helper::tokio_read_expect_client_message;
+use wow_vanilla_messages::v1::v12::opcodes::ClientOpcodeMessage;
 use wow_vanilla_messages::v1::v12::*;
-use wow_vanilla_messages::ServerMessageWrite;
+use wow_vanilla_messages::{
+    ClientMessageWrite, Guid, ServerMessageWrite, UpdateMask, UpdatePlayer,
+};
 
 pub async fn handle(mut stream: TcpStream, users: Arc<Mutex<HashMap<String, SrpServer>>>) {
     let seed = ProofSeed::new();
@@ -52,18 +53,127 @@ pub async fn handle(mut stream: TcpStream, users: Arc<Mutex<HashMap<String, SrpS
     .await
     .unwrap();
 
-    let c = tokio_read_expect_client_message_encryption::<CMSG_CHAR_ENUM, _, _>(
-        &mut stream,
-        &mut encryption,
-    )
+    loop {
+        let opcode = ClientOpcodeMessage::tokio_read_encrypted(&mut stream, &mut encryption)
+            .await
+            .unwrap();
+
+        match opcode {
+            ClientOpcodeMessage::CMSG_PING(c) => {
+                SMSG_PONG {
+                    sequence_id: c.sequence_id,
+                }
+                .tokio_write_encrypted_server(&mut stream, &mut encryption)
+                .await
+                .unwrap();
+            }
+            ClientOpcodeMessage::CMSG_CHAR_ENUM(_) => {
+                SMSG_CHAR_ENUM {
+                    characters: vec![Character {
+                        guid: Guid::new(4),
+                        name: "Warr".to_string(),
+                        race: Default::default(),
+                        class: Default::default(),
+                        gender: Default::default(),
+                        skin: 0,
+                        face: 0,
+                        hairstyle: 0,
+                        haircolor: 0,
+                        facialhair: 0,
+                        level: 0,
+                        area: Default::default(),
+                        map: Default::default(),
+                        position_x: 0.0,
+                        position_y: 0.0,
+                        position_z: 0.0,
+                        guild_id: 0,
+                        flags: Default::default(),
+                        first_login: 0,
+                        pet_display_id: 0,
+                        pet_level: 0,
+                        pet_familiy: 0,
+                        equipment: [Default::default(); 19],
+                    }],
+                }
+                .tokio_write_encrypted_server(&mut stream, &mut encryption)
+                .await
+                .unwrap();
+            }
+            ClientOpcodeMessage::CMSG_PLAYER_LOGIN(_) => {
+                break;
+            }
+            _ => panic!(),
+        }
+    }
+
+    SMSG_LOGIN_VERIFY_WORLD {
+        map: Map::KALIMDOR,
+        position_x: 200.0,
+        position_y: 200.0,
+        position_z: 200.0,
+        orientation: 0.0,
+    }
+    .tokio_write_encrypted_server(&mut stream, &mut encryption)
     .await
     .unwrap();
-    dbg!(&c);
 
-    SMSG_CHAR_ENUM { characters: vec![] }
-        .tokio_write_encrypted_server(&mut stream, &mut encryption)
-        .await
-        .unwrap();
+    SMSG_TUTORIAL_FLAGS {
+        tutorial_data0: 0xFF,
+        tutorial_data1: 0xFF,
+        tutorial_data2: 0xFF,
+        tutorial_data3: 0xFF,
+        tutorial_data4: 0xFF,
+        tutorial_data5: 0xFF,
+        tutorial_data6: 0xFF,
+        tutorial_data7: 0xFF,
+    }
+    .tokio_write_encrypted_server(&mut stream, &mut encryption)
+    .await
+    .unwrap();
 
-    sleep(Duration::new(10, 0)).await;
+    let mut update_mask = UpdatePlayer::new();
+
+    update_mask.set_object_GUID(Guid::new(4));
+    update_mask.set_object_TYPE(25);
+    update_mask.set_unit_BYTES_0(1 << 24 | 1 << 16 | 1 << 8 | 1);
+    update_mask.set_unit_HEALTH(100);
+
+    let update_flag = MovementBlockUpdateFlag::empty()
+        .set_LIVING(MovementBlockUpdateFlagLIVING::LIVING {
+            backwards_running_speed: 4.5,
+            backwards_swimming_speed: 0.0,
+            fall_time: 0.0,
+            flags: MovementBlockMovementFlags::empty(),
+            living_orientation: 0.0,
+            living_position_x: -8949.95,
+            living_position_y: -132.493,
+            living_position_z: 83.5312,
+            running_speed: 7.0,
+            swimming_speed: 0.0,
+            timestamp: 0,
+            turn_rate: 3.1415,
+            walking_speed: 1.0,
+        })
+        .set_SELF();
+
+    SMSG_UPDATE_OBJECT {
+        has_transport: 0,
+        objects: vec![Object {
+            update_type: ObjectUpdateType::CREATE_OBJECT2 {
+                guid3: Guid::new(4),
+                mask2: UpdateMask::Player(update_mask),
+                movement2: MovementBlock {
+                    update_flag: update_flag,
+                },
+                object_type: Default::default(),
+            },
+        }],
+    }
+    .tokio_write_encrypted_client(&mut stream, &mut encryption)
+    .await
+    .unwrap();
+
+    loop {
+        sleep(Duration::new(10, 0)).await;
+    }
 }
