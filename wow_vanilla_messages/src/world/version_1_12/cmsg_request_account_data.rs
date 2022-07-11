97668a91597493fd95bb400b3b230a1cfd00ccf2
Gtker <git@gtker.com>
2022-07-11 16:43:09 +0200

    Add unknown impl for CMSG_REQUEST_ACCOUNT_DATA

--- wow_vanilla_messages/src/world/version_1_12/cmsg_request_account_data.rs
+++ wow_vanilla_messages/src/world/version_1_12/cmsg_request_account_data.rs
@@ -4,33 +4,54 @@ use wow_srp::header_crypto::Encrypter;
 use std::io::{Write, Read};
 
 #[derive(Debug, PartialEq, Clone, Default)]
-#[derive(Copy)]
+/// cmangos/vmangos/mangoszero ignore the data in this.
+///
 /// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/login_logout/cmsg_request_account_data.wowm:3`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/login_logout/cmsg_request_account_data.wowm#L3):
 /// ```text
 /// cmsg CMSG_REQUEST_ACCOUNT_DATA = 0x020A {
+///     u8[-] unknown;
 /// }
 /// ```
 pub struct CMSG_REQUEST_ACCOUNT_DATA {
+    pub unknown: Vec<u8>,
 }
 
 impl ClientMessage for CMSG_REQUEST_ACCOUNT_DATA {
     fn write_into_vec(&self, w: &mut Vec<u8>) -> Result<(), std::io::Error> {
+        // unknown: u8[-]
+        for i in self.unknown.iter() {
+            w.write_all(&i.to_le_bytes())?;
+        }
+
         Ok(())
     }
     const OPCODE: u16 = 0x020a;
 
     fn client_size(&self) -> u16 {
-        6
+        (self.size() + 6) as u16
     }
 
     fn read_body(r: &mut &[u8], body_size: u32) -> std::result::Result<Self, crate::errors::ParseError> {
-        if body_size != 0 {
-            return Err(crate::errors::ParseError::InvalidSize(body_size as u32));
+        // unknown: u8[-]
+        let mut current_size = {
+            0
+        };
+        let mut unknown = Vec::with_capacity(body_size as usize - current_size);
+        while current_size < (body_size as usize) {
+            unknown.push(crate::util::read_u8_le(r)?);
+            current_size += 1;
         }
 
         Ok(Self {
+            unknown,
         })
     }
 
+}
+
+impl CMSG_REQUEST_ACCOUNT_DATA {
+    pub(crate) fn size(&self) -> usize {
+        self.unknown.len() * core::mem::size_of::<u8>() // unknown: u8[-]
+    }
 }
 
