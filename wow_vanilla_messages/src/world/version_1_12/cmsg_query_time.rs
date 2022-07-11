73b8056ac5834666c2bd42f5e7dc8634cd0afd3f
Gtker <git@gtker.com>
2022-07-11 20:40:29 +0200

    Add test/doc for CMSG_QUERY_TIME

--- wow_vanilla_messages/src/world/version_1_12/cmsg_query_time.rs
+++ wow_vanilla_messages/src/world/version_1_12/cmsg_query_time.rs
@@ -5,6 +5,8 @@ use std::io::{Write, Read};
 
 #[derive(Debug, PartialEq, Clone, Default)]
 #[derive(Copy)]
+/// Sent immediately after logging in. Client expects reply in [`SMSG_QUERY_TIME_RESPONSE`](crate::world::version_1_12::SMSG_QUERY_TIME_RESPONSE).
+///
 /// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/queries/cmsg_query_time.wowm:3`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/queries/cmsg_query_time.wowm#L3):
 /// ```text
 /// cmsg CMSG_QUERY_TIME = 0x01CE {
@@ -34,3 +36,84 @@ impl ClientMessage for CMSG_QUERY_TIME {
 
 }
 
+#[cfg(test)]
+mod test {
+    use super::CMSG_QUERY_TIME;
+    use super::*;
+    use super::super::*;
+    use crate::world::version_1_12::opcodes::ClientOpcodeMessage;
+    use crate::{Guid, UpdateMask, UpdateContainer, UpdateItem, UpdateCorpse, UpdateGameObject, UpdateDynamicObject, UpdateUnit, UpdatePlayer};
+    use crate::{ClientMessage, ServerMessage};
+
+    const RAW0: [u8; 6] = [ 0x00, 0x04, 0xCE, 0x01, 0x00, 0x00, ];
+
+    // Generated from `wow_message_parser/wowm/world/queries/cmsg_query_time.wowm` line 7.
+    #[cfg(feature = "sync")]
+    #[cfg_attr(feature = "sync", test)]
+    fn CMSG_QUERY_TIME0() {
+        let expected = CMSG_QUERY_TIME {
+        };
+
+        let header_size = 2 + 4;
+        let t = ClientOpcodeMessage::read_unencrypted(&mut std::io::Cursor::new(&RAW0)).unwrap();
+        let t = match t {
+            ClientOpcodeMessage::CMSG_QUERY_TIME(t) => t,
+            opcode => panic!("incorrect opcode. Expected CMSG_QUERY_TIME, got {opcode:#?}", opcode = opcode),
+        };
+
+
+        assert_eq!(header_size, RAW0.len());
+
+        let mut dest = Vec::with_capacity(RAW0.len());
+        expected.write_unencrypted_client(&mut std::io::Cursor::new(&mut dest)).unwrap();
+
+        assert_eq!(dest, RAW0);
+    }
+
+    // Generated from `wow_message_parser/wowm/world/queries/cmsg_query_time.wowm` line 7.
+    #[cfg(feature = "tokio")]
+    #[cfg_attr(feature = "tokio", tokio::test)]
+    async fn tokio_CMSG_QUERY_TIME0() {
+        let expected = CMSG_QUERY_TIME {
+        };
+
+        let header_size = 2 + 4;
+        let t = ClientOpcodeMessage::tokio_read_unencrypted(&mut std::io::Cursor::new(&RAW0)).await.unwrap();
+        let t = match t {
+            ClientOpcodeMessage::CMSG_QUERY_TIME(t) => t,
+            opcode => panic!("incorrect opcode. Expected CMSG_QUERY_TIME, got {opcode:#?}", opcode = opcode),
+        };
+
+
+        assert_eq!(header_size, RAW0.len());
+
+        let mut dest = Vec::with_capacity(RAW0.len());
+        expected.tokio_write_unencrypted_client(&mut std::io::Cursor::new(&mut dest)).await.unwrap();
+
+        assert_eq!(dest, RAW0);
+    }
+
+    // Generated from `wow_message_parser/wowm/world/queries/cmsg_query_time.wowm` line 7.
+    #[cfg(feature = "async-std")]
+    #[cfg_attr(feature = "async-std", async_std::test)]
+    async fn astd_CMSG_QUERY_TIME0() {
+        let expected = CMSG_QUERY_TIME {
+        };
+
+        let header_size = 2 + 4;
+        let t = ClientOpcodeMessage::astd_read_unencrypted(&mut async_std::io::Cursor::new(&RAW0)).await.unwrap();
+        let t = match t {
+            ClientOpcodeMessage::CMSG_QUERY_TIME(t) => t,
+            opcode => panic!("incorrect opcode. Expected CMSG_QUERY_TIME, got {opcode:#?}", opcode = opcode),
+        };
+
+
+        assert_eq!(header_size, RAW0.len());
+
+        let mut dest = Vec::with_capacity(RAW0.len());
+        expected.astd_write_unencrypted_client(&mut async_std::io::Cursor::new(&mut dest)).await.unwrap();
+
+        assert_eq!(dest, RAW0);
+    }
+
+}
