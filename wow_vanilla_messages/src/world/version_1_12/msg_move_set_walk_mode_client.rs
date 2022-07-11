d75e63ead804d9e649cef5d40fa5bd86eb8d93ae
Gtker <git@gtker.com>
2022-07-11 02:35:25 +0200

    Add tests for MSG_MOVE_SET_WALK_MODE_*

--- wow_vanilla_messages/src/world/version_1_12/msg_move_set_walk_mode_client.rs
+++ wow_vanilla_messages/src/world/version_1_12/msg_move_set_walk_mode_client.rs
@@ -45,3 +45,136 @@ impl MSG_MOVE_SET_WALK_MODE_Client {
     }
 }
 
+#[cfg(test)]
+mod test {
+    use super::MSG_MOVE_SET_WALK_MODE_Client;
+    use crate::world::version_1_12::MovementFlags;
+    use crate::world::version_1_12::MovementInfo;
+    use crate::world::version_1_12::TransportInfo;
+    use crate::world::version_1_12::Vector3d;
+    use super::*;
+    use super::super::*;
+    use crate::world::version_1_12::opcodes::ClientOpcodeMessage;
+    use crate::{Guid, UpdateMask, UpdateContainer, UpdateItem, UpdateCorpse, UpdateGameObject, UpdateDynamicObject, UpdateUnit, UpdatePlayer};
+    use crate::{ClientMessage, ServerMessage};
+
+    const RAW0: [u8; 34] = [ 0x00, 0x20, 0xC3, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00,
+         0x00, 0x9A, 0x17, 0x5B, 0x02, 0x02, 0xBD, 0x0B, 0xC6, 0x4E, 0x58, 0x01,
+         0xC3, 0x26, 0x29, 0xA7, 0x42, 0x2E, 0x0E, 0xC3, 0x40, 0x00, 0x00, 0x00,
+         0x00, ];
+
+    // Generated from `wow_message_parser/wowm/world/movement/msg/msg_move_set_walk_mode.wowm` line 7.
+    #[cfg(feature = "sync")]
+    #[cfg_attr(feature = "sync", test)]
+    fn MSG_MOVE_SET_WALK_MODE_Client0() {
+        let expected = MSG_MOVE_SET_WALK_MODE_Client {
+            info: MovementInfo {
+                flags: MovementInfo_MovementFlags::empty()
+                    .set_WALK_MODE()
+                    .set_FORWARD()
+                    ,
+                timestamp: 0x25B179A,
+                position: Vector3d {
+                    x: -8943.252_f32,
+                    y: -129.34494_f32,
+                    z: 83.58037_f32,
+                },
+                orientation: 6.095481_f32,
+                fall_time: 0_f32,
+            },
+        };
+
+        let header_size = 2 + 4;
+        let t = ClientOpcodeMessage::read_unencrypted(&mut std::io::Cursor::new(&RAW0)).unwrap();
+        let t = match t {
+            ClientOpcodeMessage::MSG_MOVE_SET_WALK_MODE(t) => t,
+            opcode => panic!("incorrect opcode. Expected MSG_MOVE_SET_WALK_MODE, got {opcode:#?}", opcode = opcode),
+        };
+
+        assert_eq!(t.info, expected.info);
+
+        assert_eq!(t.size() + header_size, RAW0.len());
+
+        let mut dest = Vec::with_capacity(RAW0.len());
+        expected.write_unencrypted_client(&mut std::io::Cursor::new(&mut dest)).unwrap();
+
+        assert_eq!(dest, RAW0);
+    }
+
+    // Generated from `wow_message_parser/wowm/world/movement/msg/msg_move_set_walk_mode.wowm` line 7.
+    #[cfg(feature = "tokio")]
+    #[cfg_attr(feature = "tokio", tokio::test)]
+    async fn tokio_MSG_MOVE_SET_WALK_MODE_Client0() {
+        let expected = MSG_MOVE_SET_WALK_MODE_Client {
+            info: MovementInfo {
+                flags: MovementInfo_MovementFlags::empty()
+                    .set_WALK_MODE()
+                    .set_FORWARD()
+                    ,
+                timestamp: 0x25B179A,
+                position: Vector3d {
+                    x: -8943.252_f32,
+                    y: -129.34494_f32,
+                    z: 83.58037_f32,
+                },
+                orientation: 6.095481_f32,
+                fall_time: 0_f32,
+            },
+        };
+
+        let header_size = 2 + 4;
+        let t = ClientOpcodeMessage::tokio_read_unencrypted(&mut std::io::Cursor::new(&RAW0)).await.unwrap();
+        let t = match t {
+            ClientOpcodeMessage::MSG_MOVE_SET_WALK_MODE(t) => t,
+            opcode => panic!("incorrect opcode. Expected MSG_MOVE_SET_WALK_MODE, got {opcode:#?}", opcode = opcode),
+        };
+
+        assert_eq!(t.info, expected.info);
+
+        assert_eq!(t.size() + header_size, RAW0.len());
+
+        let mut dest = Vec::with_capacity(RAW0.len());
+        expected.tokio_write_unencrypted_client(&mut std::io::Cursor::new(&mut dest)).await.unwrap();
+
+        assert_eq!(dest, RAW0);
+    }
+
+    // Generated from `wow_message_parser/wowm/world/movement/msg/msg_move_set_walk_mode.wowm` line 7.
+    #[cfg(feature = "async-std")]
+    #[cfg_attr(feature = "async-std", async_std::test)]
+    async fn astd_MSG_MOVE_SET_WALK_MODE_Client0() {
+        let expected = MSG_MOVE_SET_WALK_MODE_Client {
+            info: MovementInfo {
+                flags: MovementInfo_MovementFlags::empty()
+                    .set_WALK_MODE()
+                    .set_FORWARD()
+                    ,
+                timestamp: 0x25B179A,
+                position: Vector3d {
+                    x: -8943.252_f32,
+                    y: -129.34494_f32,
+                    z: 83.58037_f32,
+                },
+                orientation: 6.095481_f32,
+                fall_time: 0_f32,
+            },
+        };
+
+        let header_size = 2 + 4;
+        let t = ClientOpcodeMessage::astd_read_unencrypted(&mut async_std::io::Cursor::new(&RAW0)).await.unwrap();
+        let t = match t {
+            ClientOpcodeMessage::MSG_MOVE_SET_WALK_MODE(t) => t,
+            opcode => panic!("incorrect opcode. Expected MSG_MOVE_SET_WALK_MODE, got {opcode:#?}", opcode = opcode),
+        };
+
+        assert_eq!(t.info, expected.info);
+
+        assert_eq!(t.size() + header_size, RAW0.len());
+
+        let mut dest = Vec::with_capacity(RAW0.len());
+        expected.astd_write_unencrypted_client(&mut async_std::io::Cursor::new(&mut dest)).await.unwrap();
+
+        assert_eq!(dest, RAW0);
+    }
+
+}
