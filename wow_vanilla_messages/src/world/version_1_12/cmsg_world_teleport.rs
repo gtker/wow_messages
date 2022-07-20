29f792c48e775244ee1e1cb4c505153296adefda
Gtker <git@gtker.com>
2022-07-20 22:41:30 +0200

    Fix CMSG_WORLD_TELEPORT incorrect u64

--- wow_vanilla_messages/src/world/version_1_12/cmsg_world_teleport.rs
+++ wow_vanilla_messages/src/world/version_1_12/cmsg_world_teleport.rs
@@ -7,17 +7,19 @@ use std::io::{Write, Read};
 
 #[derive(Debug, PartialEq, Clone, Default)]
 #[derive(Copy)]
+/// Sent when using the `worldport` console command.
+///
 /// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/movement/cmsg/cmsg_world_teleport.wowm:3`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/movement/cmsg/cmsg_world_teleport.wowm#L3):
 /// ```text
 /// cmsg CMSG_WORLD_TELEPORT = 0x0008 {
-///     u64 time_in_msec;
+///     u32 time_in_msec;
 ///     Map map;
 ///     Vector3d position;
 ///     f32 orientation;
 /// }
 /// ```
 pub struct CMSG_WORLD_TELEPORT {
-    pub time_in_msec: u64,
+    pub time_in_msec: u32,
     pub map: Map,
     pub position: Vector3d,
     pub orientation: f32,
@@ -25,7 +27,7 @@ pub struct CMSG_WORLD_TELEPORT {
 
 impl ClientMessage for CMSG_WORLD_TELEPORT {
     fn write_into_vec(&self, w: &mut Vec<u8>) -> Result<(), std::io::Error> {
-        // time_in_msec: u64
+        // time_in_msec: u32
         w.write_all(&self.time_in_msec.to_le_bytes())?;
 
         // map: Map
@@ -42,16 +44,16 @@ impl ClientMessage for CMSG_WORLD_TELEPORT {
     const OPCODE: u16 = 0x0008;
 
     fn client_size(&self) -> u16 {
-        34
+        30
     }
 
     fn read_body(r: &mut &[u8], body_size: u32) -> std::result::Result<Self, crate::errors::ParseError> {
-        if body_size != 28 {
+        if body_size != 24 {
             return Err(crate::errors::ParseError::InvalidSize(body_size as u32));
         }
 
-        // time_in_msec: u64
-        let time_in_msec = crate::util::read_u64_le(r)?;
+        // time_in_msec: u32
+        let time_in_msec = crate::util::read_u32_le(r)?;
 
         // map: Map
         let map: Map = map_try_from(crate::util::read_u32_le(r)?)?;
@@ -82,17 +84,16 @@ mod test {
     use crate::{Guid, UpdateMask, UpdateContainer, UpdateItem, UpdateCorpse, UpdateGameObject, UpdateDynamicObject, UpdateUnit, UpdatePlayer};
     use crate::{ClientMessage, ServerMessage};
 
-    const RAW0: [u8; 34] = [ 0x00, 0x20, 0x08, 0x00, 0x00, 0x00, 0xEF, 0xBE, 0xAD,
-         0xDE, 0xDE, 0xCA, 0xFA, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80,
-         0x3F, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x40, 0x40, 0x00, 0x00, 0x80,
-         0x40, ];
+    const RAW0: [u8; 30] = [ 0x00, 0x1C, 0x08, 0x00, 0x00, 0x00, 0xEF, 0xBE, 0xAD,
+         0xDE, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x3F, 0x00, 0x00, 0x00,
+         0x40, 0x00, 0x00, 0x40, 0x40, 0x00, 0x00, 0x80, 0x40, ];
 
-    // Generated from `wow_message_parser/wowm/world/movement/cmsg/cmsg_world_teleport.wowm` line 10.
+    // Generated from `wow_message_parser/wowm/world/movement/cmsg/cmsg_world_teleport.wowm` line 12.
     #[cfg(feature = "sync")]
     #[cfg_attr(feature = "sync", test)]
     fn CMSG_WORLD_TELEPORT0() {
         let expected = CMSG_WORLD_TELEPORT {
-            time_in_msec: 0xFACADEDEADBEEF,
+            time_in_msec: 0xDEADBEEF,
             map: Map::KALIMDOR,
             position: Vector3d {
                 x: 1_f32,
@@ -114,7 +115,7 @@ mod test {
         assert_eq!(t.position, expected.position);
         assert_eq!(t.orientation, expected.orientation);
 
-        assert_eq!(28 + header_size, RAW0.len());
+        assert_eq!(24 + header_size, RAW0.len());
 
         let mut dest = Vec::with_capacity(RAW0.len());
         expected.write_unencrypted_client(&mut std::io::Cursor::new(&mut dest)).unwrap();
@@ -122,12 +123,12 @@ mod test {
         assert_eq!(dest, RAW0);
     }
 
-    // Generated from `wow_message_parser/wowm/world/movement/cmsg/cmsg_world_teleport.wowm` line 10.
+    // Generated from `wow_message_parser/wowm/world/movement/cmsg/cmsg_world_teleport.wowm` line 12.
     #[cfg(feature = "tokio")]
     #[cfg_attr(feature = "tokio", tokio::test)]
     async fn tokio_CMSG_WORLD_TELEPORT0() {
         let expected = CMSG_WORLD_TELEPORT {
-            time_in_msec: 0xFACADEDEADBEEF,
+            time_in_msec: 0xDEADBEEF,
             map: Map::KALIMDOR,
             position: Vector3d {
                 x: 1_f32,
@@ -149,7 +150,7 @@ mod test {
         assert_eq!(t.position, expected.position);
         assert_eq!(t.orientation, expected.orientation);
 
-        assert_eq!(28 + header_size, RAW0.len());
+        assert_eq!(24 + header_size, RAW0.len());
 
         let mut dest = Vec::with_capacity(RAW0.len());
         expected.tokio_write_unencrypted_client(&mut std::io::Cursor::new(&mut dest)).await.unwrap();
@@ -157,12 +158,12 @@ mod test {
         assert_eq!(dest, RAW0);
     }
 
-    // Generated from `wow_message_parser/wowm/world/movement/cmsg/cmsg_world_teleport.wowm` line 10.
+    // Generated from `wow_message_parser/wowm/world/movement/cmsg/cmsg_world_teleport.wowm` line 12.
     #[cfg(feature = "async-std")]
     #[cfg_attr(feature = "async-std", async_std::test)]
     async fn astd_CMSG_WORLD_TELEPORT0() {
         let expected = CMSG_WORLD_TELEPORT {
-            time_in_msec: 0xFACADEDEADBEEF,
+            time_in_msec: 0xDEADBEEF,
             map: Map::KALIMDOR,
             position: Vector3d {
                 x: 1_f32,
@@ -184,7 +185,7 @@ mod test {
         assert_eq!(t.position, expected.position);
         assert_eq!(t.orientation, expected.orientation);
 
-        assert_eq!(28 + header_size, RAW0.len());
+        assert_eq!(24 + header_size, RAW0.len());
 
         let mut dest = Vec::with_capacity(RAW0.len());
         expected.astd_write_unencrypted_client(&mut async_std::io::Cursor::new(&mut dest)).await.unwrap();
