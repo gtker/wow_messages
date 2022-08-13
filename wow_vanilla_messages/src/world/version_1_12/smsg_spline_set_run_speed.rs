3d388ced9a445edc4d02ac3629919ba27cd77cee
Gtker <git@gtker.com>
2022-08-13 18:15:33 +0200

    Add test for SMSG_FORCE_RUN_SPEED_CHANGE

--- wow_vanilla_messages/src/world/version_1_12/smsg_spline_set_run_speed.rs
+++ wow_vanilla_messages/src/world/version_1_12/smsg_spline_set_run_speed.rs
@@ -5,6 +5,9 @@ use wow_srp::header_crypto::Encrypter;
 use std::io::{Write, Read};
 
 #[derive(Debug, PartialEq, Clone, Default)]
+/// Informs the client that the run speed of a unit has changed.
+/// Mangos sends this to third parties that aren't having their speed changed and [`SMSG_FORCE_RUN_SPEED_CHANGE`](crate::world::version_1_12::SMSG_FORCE_RUN_SPEED_CHANGE) to the client that has their run speed changed.
+///
 /// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/movement/smsg/smsg_spline_run_speed.wowm:3`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/movement/smsg/smsg_spline_run_speed.wowm#L3):
 /// ```text
 /// smsg SMSG_SPLINE_SET_RUN_SPEED = 0x02FE {
@@ -54,3 +57,97 @@ impl SMSG_SPLINE_SET_RUN_SPEED {
     }
 }
 
+#[cfg(test)]
+mod test {
+    use super::SMSG_SPLINE_SET_RUN_SPEED;
+    use super::*;
+    use super::super::*;
+    use crate::world::version_1_12::opcodes::ServerOpcodeMessage;
+    use crate::{Guid, UpdateMask, UpdateContainer, UpdateItem, UpdateCorpse, UpdateGameObject, UpdateDynamicObject, UpdateUnit, UpdatePlayer};
+    use crate::{ClientMessage, ServerMessage};
+
+    const RAW0: [u8; 10] = [ 0x00, 0x08, 0xFE, 0x02, 0x01, 0x06, 0x00, 0x00, 0xE0,
+         0x40, ];
+
+    // Generated from `wow_message_parser/wowm/world/movement/smsg/smsg_spline_run_speed.wowm` line 11.
+    #[cfg(feature = "sync")]
+    #[cfg_attr(feature = "sync", test)]
+    fn SMSG_SPLINE_SET_RUN_SPEED0() {
+        let expected = SMSG_SPLINE_SET_RUN_SPEED {
+            guid: Guid::new(0x6),
+            speed: 7_f32,
+        };
+
+        let header_size = 2 + 2;
+        let t = ServerOpcodeMessage::read_unencrypted(&mut std::io::Cursor::new(&RAW0)).unwrap();
+        let t = match t {
+            ServerOpcodeMessage::SMSG_SPLINE_SET_RUN_SPEED(t) => t,
+            opcode => panic!("incorrect opcode. Expected SMSG_SPLINE_SET_RUN_SPEED, got {opcode:#?}", opcode = opcode),
+        };
+
+        assert_eq!(t.guid, expected.guid);
+        assert_eq!(t.speed, expected.speed);
+
+        assert_eq!(t.size() + header_size, RAW0.len());
+
+        let mut dest = Vec::with_capacity(RAW0.len());
+        expected.write_unencrypted_server(&mut std::io::Cursor::new(&mut dest)).unwrap();
+
+        assert_eq!(dest, RAW0);
+    }
+
+    // Generated from `wow_message_parser/wowm/world/movement/smsg/smsg_spline_run_speed.wowm` line 11.
+    #[cfg(feature = "tokio")]
+    #[cfg_attr(feature = "tokio", tokio::test)]
+    async fn tokio_SMSG_SPLINE_SET_RUN_SPEED0() {
+        let expected = SMSG_SPLINE_SET_RUN_SPEED {
+            guid: Guid::new(0x6),
+            speed: 7_f32,
+        };
+
+        let header_size = 2 + 2;
+        let t = ServerOpcodeMessage::tokio_read_unencrypted(&mut std::io::Cursor::new(&RAW0)).await.unwrap();
+        let t = match t {
+            ServerOpcodeMessage::SMSG_SPLINE_SET_RUN_SPEED(t) => t,
+            opcode => panic!("incorrect opcode. Expected SMSG_SPLINE_SET_RUN_SPEED, got {opcode:#?}", opcode = opcode),
+        };
+
+        assert_eq!(t.guid, expected.guid);
+        assert_eq!(t.speed, expected.speed);
+
+        assert_eq!(t.size() + header_size, RAW0.len());
+
+        let mut dest = Vec::with_capacity(RAW0.len());
+        expected.tokio_write_unencrypted_server(&mut std::io::Cursor::new(&mut dest)).await.unwrap();
+
+        assert_eq!(dest, RAW0);
+    }
+
+    // Generated from `wow_message_parser/wowm/world/movement/smsg/smsg_spline_run_speed.wowm` line 11.
+    #[cfg(feature = "async-std")]
+    #[cfg_attr(feature = "async-std", async_std::test)]
+    async fn astd_SMSG_SPLINE_SET_RUN_SPEED0() {
+        let expected = SMSG_SPLINE_SET_RUN_SPEED {
+            guid: Guid::new(0x6),
+            speed: 7_f32,
+        };
+
+        let header_size = 2 + 2;
+        let t = ServerOpcodeMessage::astd_read_unencrypted(&mut async_std::io::Cursor::new(&RAW0)).await.unwrap();
+        let t = match t {
+            ServerOpcodeMessage::SMSG_SPLINE_SET_RUN_SPEED(t) => t,
+            opcode => panic!("incorrect opcode. Expected SMSG_SPLINE_SET_RUN_SPEED, got {opcode:#?}", opcode = opcode),
+        };
+
+        assert_eq!(t.guid, expected.guid);
+        assert_eq!(t.speed, expected.speed);
+
+        assert_eq!(t.size() + header_size, RAW0.len());
+
+        let mut dest = Vec::with_capacity(RAW0.len());
+        expected.astd_write_unencrypted_server(&mut async_std::io::Cursor::new(&mut dest)).await.unwrap();
+
+        assert_eq!(dest, RAW0);
+    }
+
+}
