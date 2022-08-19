22a24c34c504898593eac90e3575fa5a0bbfc2c7
Gtker <git@gtker.com>
2022-08-19 18:52:34 +0200

    Add test for SMSG_DESTROY_OBJECT

--- wow_vanilla_messages/src/world/version_1_12/smsg_destroy_object.rs
+++ wow_vanilla_messages/src/world/version_1_12/smsg_destroy_object.rs
@@ -6,6 +6,10 @@ use std::io::{Write, Read};
 
 #[derive(Debug, PartialEq, Clone, Default)]
 #[derive(Copy)]
+/// Immediately removes an object from the presence of the player.
+///
+/// Used by vmangos for logout.
+///
 /// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/gameobject/smsg_destroy_object.wowm:3`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/gameobject/smsg_destroy_object.wowm#L3):
 /// ```text
 /// smsg SMSG_DESTROY_OBJECT = 0x00AA {
@@ -44,3 +48,91 @@ impl ServerMessage for SMSG_DESTROY_OBJECT {
 
 }
 
+#[cfg(test)]
+mod test {
+    use super::SMSG_DESTROY_OBJECT;
+    use super::*;
+    use super::super::*;
+    use crate::world::version_1_12::opcodes::ServerOpcodeMessage;
+    use crate::{Guid, UpdateMask, UpdateContainer, UpdateItem, UpdateCorpse, UpdateGameObject, UpdateDynamicObject, UpdateUnit, UpdatePlayer};
+    use crate::{ClientMessage, ServerMessage};
+
+    const RAW0: [u8; 12] = [ 0x00, 0x0A, 0xAA, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00,
+         0x00, 0x00, 0x00, ];
+
+    // Generated from `wow_message_parser/wowm/world/gameobject/smsg_destroy_object.wowm` line 10.
+    #[cfg(feature = "sync")]
+    #[cfg_attr(feature = "sync", test)]
+    fn SMSG_DESTROY_OBJECT0() {
+        let expected = SMSG_DESTROY_OBJECT {
+            guid: Guid::new(0x6),
+        };
+
+        let header_size = 2 + 2;
+        let t = ServerOpcodeMessage::read_unencrypted(&mut std::io::Cursor::new(&RAW0)).unwrap();
+        let t = match t {
+            ServerOpcodeMessage::SMSG_DESTROY_OBJECT(t) => t,
+            opcode => panic!("incorrect opcode. Expected SMSG_DESTROY_OBJECT, got {opcode:#?}", opcode = opcode),
+        };
+
+        assert_eq!(t.guid, expected.guid);
+
+        assert_eq!(8 + header_size, RAW0.len());
+
+        let mut dest = Vec::with_capacity(RAW0.len());
+        expected.write_unencrypted_server(&mut std::io::Cursor::new(&mut dest)).unwrap();
+
+        assert_eq!(dest, RAW0);
+    }
+
+    // Generated from `wow_message_parser/wowm/world/gameobject/smsg_destroy_object.wowm` line 10.
+    #[cfg(feature = "tokio")]
+    #[cfg_attr(feature = "tokio", tokio::test)]
+    async fn tokio_SMSG_DESTROY_OBJECT0() {
+        let expected = SMSG_DESTROY_OBJECT {
+            guid: Guid::new(0x6),
+        };
+
+        let header_size = 2 + 2;
+        let t = ServerOpcodeMessage::tokio_read_unencrypted(&mut std::io::Cursor::new(&RAW0)).await.unwrap();
+        let t = match t {
+            ServerOpcodeMessage::SMSG_DESTROY_OBJECT(t) => t,
+            opcode => panic!("incorrect opcode. Expected SMSG_DESTROY_OBJECT, got {opcode:#?}", opcode = opcode),
+        };
+
+        assert_eq!(t.guid, expected.guid);
+
+        assert_eq!(8 + header_size, RAW0.len());
+
+        let mut dest = Vec::with_capacity(RAW0.len());
+        expected.tokio_write_unencrypted_server(&mut std::io::Cursor::new(&mut dest)).await.unwrap();
+
+        assert_eq!(dest, RAW0);
+    }
+
+    // Generated from `wow_message_parser/wowm/world/gameobject/smsg_destroy_object.wowm` line 10.
+    #[cfg(feature = "async-std")]
+    #[cfg_attr(feature = "async-std", async_std::test)]
+    async fn astd_SMSG_DESTROY_OBJECT0() {
+        let expected = SMSG_DESTROY_OBJECT {
+            guid: Guid::new(0x6),
+        };
+
+        let header_size = 2 + 2;
+        let t = ServerOpcodeMessage::astd_read_unencrypted(&mut async_std::io::Cursor::new(&RAW0)).await.unwrap();
+        let t = match t {
+            ServerOpcodeMessage::SMSG_DESTROY_OBJECT(t) => t,
+            opcode => panic!("incorrect opcode. Expected SMSG_DESTROY_OBJECT, got {opcode:#?}", opcode = opcode),
+        };
+
+        assert_eq!(t.guid, expected.guid);
+
+        assert_eq!(8 + header_size, RAW0.len());
+
+        let mut dest = Vec::with_capacity(RAW0.len());
+        expected.astd_write_unencrypted_server(&mut async_std::io::Cursor::new(&mut dest)).await.unwrap();
+
+        assert_eq!(dest, RAW0);
+    }
+
+}
