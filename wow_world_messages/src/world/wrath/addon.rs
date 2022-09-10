5b2e97abfdc083b39dcd848533ba981027a4b48f
victov <victor.veldstra@gmail.com>
2022-09-10 11:26:08 +0200

    More review fixes

--- wow_world_messages/src/world/wrath/addon.rs
+++ wow_world_messages/src/world/wrath/addon.rs
@@ -5,14 +5,20 @@ use std::io::{Write, Read};
 /// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/login_logout/smsg_addon_info.wowm:1`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/login_logout/smsg_addon_info.wowm#L1):
 /// ```text
 /// struct Addon {
-///     u8 addon_type = 2;
-///     u8 uses_crc = 1;
+///     u8 addon_type;
+///     u8 uses_crc;
 ///     u8 uses_diffent_public_key;
 ///     u32 unknown1;
 ///     u8 unknown2;
 /// }
 /// ```
 pub struct Addon {
+    /// Other emus hardcode this to 2. More research is required
+    ///
+    pub addon_type: u8,
+    /// Other emus hardcode this to 1.
+    ///
+    pub uses_crc: u8,
     pub uses_diffent_public_key: u8,
     /// Other emus hardcode this to 0
     ///
@@ -23,37 +29,12 @@ pub struct Addon {
 }
 
 impl Addon {
-    /// The field `addon_type` is constantly specified to be:
-    ///
-    /// | Format | Value |
-    /// | ------ | ----- |
-    /// | Decimal | `2` |
-    /// | Hex | `0x02` |
-    /// | Original | `2` |
-    ///
-    /// **This field is not in the Rust struct, but is written as this constant value.**
-    pub const ADDON_TYPE_VALUE: u8 = 0x02;
-
-    /// The field `uses_crc` is constantly specified to be:
-    ///
-    /// | Format | Value |
-    /// | ------ | ----- |
-    /// | Decimal | `1` |
-    /// | Hex | `0x01` |
-    /// | Original | `1` |
-    ///
-    /// **This field is not in the Rust struct, but is written as this constant value.**
-    pub const USES_CRC_VALUE: u8 = 0x01;
-
-}
-
-impl Addon {
     pub(crate) fn write_into_vec(&self, w: &mut Vec<u8>) -> Result<(), std::io::Error> {
         // addon_type: u8
-        w.write_all(&Self::ADDON_TYPE_VALUE.to_le_bytes())?;
+        w.write_all(&self.addon_type.to_le_bytes())?;
 
         // uses_crc: u8
-        w.write_all(&Self::USES_CRC_VALUE.to_le_bytes())?;
+        w.write_all(&self.uses_crc.to_le_bytes())?;
 
         // uses_diffent_public_key: u8
         w.write_all(&self.uses_diffent_public_key.to_le_bytes())?;
@@ -71,12 +52,10 @@ impl Addon {
 impl Addon {
     pub(crate) fn read<R: std::io::Read>(r: &mut R) -> std::result::Result<Self, crate::errors::ParseError> {
         // addon_type: u8
-        let _addon_type = crate::util::read_u8_le(r)?;
-        // addon_type is expected to always be 2 (2)
+        let addon_type = crate::util::read_u8_le(r)?;
 
         // uses_crc: u8
-        let _uses_crc = crate::util::read_u8_le(r)?;
-        // uses_crc is expected to always be 1 (1)
+        let uses_crc = crate::util::read_u8_le(r)?;
 
         // uses_diffent_public_key: u8
         let uses_diffent_public_key = crate::util::read_u8_le(r)?;
@@ -88,6 +67,8 @@ impl Addon {
         let unknown2 = crate::util::read_u8_le(r)?;
 
         Ok(Self {
+            addon_type,
+            uses_crc,
             uses_diffent_public_key,
             unknown1,
             unknown2,
