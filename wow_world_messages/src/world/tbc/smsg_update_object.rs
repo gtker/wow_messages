f09b6572c27158ae05b8b75096bae6b1cbb3b979
victov <victor.veldstra@gmail.com>
2022-10-09 13:16:19 +0200

    Fix incorrect slashes on Windows and regenerate files

--- wow_world_messages/src/world/tbc/smsg_update_object.rs
+++ wow_world_messages/src/world/tbc/smsg_update_object.rs
@@ -3,7 +3,7 @@ use crate::world::tbc::Object;
 use std::io::{Write, Read};
 
 #[derive(Debug, Clone, PartialEq, Default)]
-/// Auto generated from the original `wowm` in file [`wow_message_parser\wowm\world\gameobject\smsg_update_object_2_4_3.wowm:165`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser\wowm\world\gameobject\smsg_update_object_2_4_3.wowm#L165):
+/// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/gameobject/smsg_update_object_2_4_3.wowm:165`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/gameobject/smsg_update_object_2_4_3.wowm#L165):
 /// ```text
 /// smsg SMSG_UPDATE_OBJECT = 0x00A9 {
 ///     u32 amount_of_objects;
