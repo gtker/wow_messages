95183391c15650de2438dd1db6d774d712a62ed0
Gtker <git@gtker.com>
2022-10-09 23:33:07 +0200

    doc: Add support for optional in examples

--- wow_message_parser/src/doc_printer/container.rs
+++ wow_message_parser/src/doc_printer/container.rs
@@ -332,8 +332,12 @@ fn print_container_example_member(
                 }
             }
         }
-        StructMember::OptionalStatement(_) => {
-            unimplemented!("UNIMPLEMENTED_DOC_OPTIONAL");
+        StructMember::OptionalStatement(optional) => {
+            s.wln(format!("// Optional {}", optional.name()));
+
+            for m in optional.members() {
+                print_container_example_member(s, e, m, bytes, values, o, tags, prefix);
+            }
         }
     }
 }
