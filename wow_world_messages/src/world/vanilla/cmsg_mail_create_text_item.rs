8ba894c8b10b4beb56e8622f229c33d0f1556a53
Gtker <git@gtker.com>
2022-10-26 17:27:47 +0200

    Rename 'mailbox_guid' variables to 'mailbox'

--- wow_world_messages/src/world/vanilla/cmsg_mail_create_text_item.rs
+++ wow_world_messages/src/world/vanilla/cmsg_mail_create_text_item.rs
@@ -6,13 +6,13 @@ use std::io::{Write, Read};
 /// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/mail/cmsg_mail_create_text_item.wowm:3`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/mail/cmsg_mail_create_text_item.wowm#L3):
 /// ```text
 /// cmsg CMSG_MAIL_CREATE_TEXT_ITEM = 0x024A {
-///     Guid mailbox_guid;
+///     Guid mailbox;
 ///     u32 mail_id;
 ///     u32 mail_template_id;
 /// }
 /// ```
 pub struct CMSG_MAIL_CREATE_TEXT_ITEM {
-    pub mailbox_guid: Guid,
+    pub mailbox: Guid,
     pub mail_id: u32,
     /// mangoszero/cmangos/vmangos: mailTemplateId, non need, Mail store own 100% correct value anyway
     ///
@@ -27,8 +27,8 @@ impl crate::Message for CMSG_MAIL_CREATE_TEXT_ITEM {
     }
 
     fn write_into_vec(&self, w: &mut Vec<u8>) -> Result<(), std::io::Error> {
-        // mailbox_guid: Guid
-        w.write_all(&self.mailbox_guid.guid().to_le_bytes())?;
+        // mailbox: Guid
+        w.write_all(&self.mailbox.guid().to_le_bytes())?;
 
         // mail_id: u32
         w.write_all(&self.mail_id.to_le_bytes())?;
@@ -43,8 +43,8 @@ impl crate::Message for CMSG_MAIL_CREATE_TEXT_ITEM {
             return Err(crate::errors::ParseError::InvalidSize(body_size as u32));
         }
 
-        // mailbox_guid: Guid
-        let mailbox_guid = Guid::read(r)?;
+        // mailbox: Guid
+        let mailbox = Guid::read(r)?;
 
         // mail_id: u32
         let mail_id = crate::util::read_u32_le(r)?;
@@ -53,7 +53,7 @@ impl crate::Message for CMSG_MAIL_CREATE_TEXT_ITEM {
         let mail_template_id = crate::util::read_u32_le(r)?;
 
         Ok(Self {
-            mailbox_guid,
+            mailbox,
             mail_id,
             mail_template_id,
         })
--- wow_world_messages/src/world/vanilla/cmsg_mail_take_item.rs
+++ wow_world_messages/src/world/vanilla/cmsg_mail_take_item.rs
@@ -6,12 +6,12 @@ use std::io::{Write, Read};
 /// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/mail/cmsg_mail_take_item.wowm:3`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/mail/cmsg_mail_take_item.wowm#L3):
 /// ```text
 /// cmsg CMSG_MAIL_TAKE_ITEM = 0x0246 {
-///     Guid mailbox_guid;
+///     Guid mailbox;
 ///     u32 mail_id;
 /// }
 /// ```
 pub struct CMSG_MAIL_TAKE_ITEM {
-    pub mailbox_guid: Guid,
+    pub mailbox: Guid,
     pub mail_id: u32,
 }
 
@@ -23,8 +23,8 @@ impl crate::Message for CMSG_MAIL_TAKE_ITEM {
     }
 
     fn write_into_vec(&self, w: &mut Vec<u8>) -> Result<(), std::io::Error> {
-        // mailbox_guid: Guid
-        w.write_all(&self.mailbox_guid.guid().to_le_bytes())?;
+        // mailbox: Guid
+        w.write_all(&self.mailbox.guid().to_le_bytes())?;
 
         // mail_id: u32
         w.write_all(&self.mail_id.to_le_bytes())?;
@@ -36,14 +36,14 @@ impl crate::Message for CMSG_MAIL_TAKE_ITEM {
             return Err(crate::errors::ParseError::InvalidSize(body_size as u32));
         }
 
-        // mailbox_guid: Guid
-        let mailbox_guid = Guid::read(r)?;
+        // mailbox: Guid
+        let mailbox = Guid::read(r)?;
 
         // mail_id: u32
         let mail_id = crate::util::read_u32_le(r)?;
 
         Ok(Self {
-            mailbox_guid,
+            mailbox,
             mail_id,
         })
     }
