51c32b551ae1e9bf04271dd326aea2da38af4b4f
Gtker <git@gtker.com>
2022-11-02 21:19:45 +0100

    parser: Fix clippy lints

--- wow_message_parser/src/parser/types/container.rs
+++ wow_message_parser/src/parser/types/container.rs
@@ -42,10 +42,7 @@ impl ContainerType {
     }
 
     pub(crate) fn is_top_level(&self) -> bool {
-        match self {
-            ContainerType::Struct => false,
-            _ => true,
-        }
+        self != &ContainerType::Struct
     }
 }
 
