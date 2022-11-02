a73cb0cea887cc4261c3bcb56a742a6c606d6926
Gtker <git@gtker.com>
2022-11-02 16:48:09 +0100

    world: Move UpdateMask impls into macro
    They were all duplicates which could lead to inconsistencies.

--- wow_world_messages/src/helper/update_mask_common.rs
+++ wow_world_messages/src/helper/update_mask_common.rs
@@ -214,4 +214,87 @@ macro_rules! update_item {
     };
 }
 
+macro_rules! update_mask {
+    () => {
+        #[derive(Debug, Hash, Clone, PartialEq, Eq)]
+        pub enum UpdateMask {
+            Item(UpdateItem),
+            Container(UpdateContainer),
+            Unit(UpdateUnit),
+            Player(UpdatePlayer),
+            GameObject(UpdateGameObject),
+            DynamicObject(UpdateDynamicObject),
+            Corpse(UpdateCorpse),
+        }
+
+        impl Default for UpdateMask {
+            fn default() -> Self {
+                Self::Item(Default::default())
+            }
+        }
+
+        impl UpdateMask {
+            pub(crate) fn read(r: &mut impl Read) -> Result<Self, io::Error> {
+                let (header, values) = update_mask_common::read_inner(r)?;
+
+                let ty = match values.get(&2) {
+                    None => {
+                        return Err(std::io::Error::new(
+                            std::io::ErrorKind::Other,
+                            "Missing object TYPE",
+                        ))
+                    }
+                    Some(ty) => *ty,
+                };
+
+                Ok(if (ty & CONTAINER) != 0 {
+                    Self::Container(UpdateContainer::from_inners(header, values))
+                } else if (ty & ITEM) != 0 {
+                    Self::Item(UpdateItem::from_inners(header, values))
+                } else if (ty & PLAYER) != 0 {
+                    Self::Player(UpdatePlayer::from_inners(header, values))
+                } else if (ty & UNIT) != 0 {
+                    Self::Unit(UpdateUnit::from_inners(header, values))
+                } else if (ty & GAMEOBJECT) != 0 {
+                    Self::GameObject(UpdateGameObject::from_inners(header, values))
+                } else if (ty & DYNAMICOBJECT) != 0 {
+                    Self::DynamicObject(UpdateDynamicObject::from_inners(header, values))
+                } else if (ty & CORPSE) != 0 {
+                    Self::Corpse(UpdateCorpse::from_inners(header, values))
+                } else {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::Other,
+                        "Object type not valid",
+                    ));
+                })
+            }
+
+            pub(crate) fn write_into_vec(&self, v: &mut Vec<u8>) -> Result<(), std::io::Error> {
+                match self {
+                    UpdateMask::Item(i) => i.write_into_vec(v),
+                    UpdateMask::Container(i) => i.write_into_vec(v),
+                    UpdateMask::Unit(i) => i.write_into_vec(v),
+                    UpdateMask::Player(i) => i.write_into_vec(v),
+                    UpdateMask::GameObject(i) => i.write_into_vec(v),
+                    UpdateMask::DynamicObject(i) => i.write_into_vec(v),
+                    UpdateMask::Corpse(i) => i.write_into_vec(v),
+                }
+            }
+
+            pub(crate) fn size(&self) -> usize {
+                match self {
+                    UpdateMask::Item(i) => update_mask_size(&i.dirty_mask),
+                    UpdateMask::Container(i) => update_mask_size(&i.dirty_mask),
+                    UpdateMask::Unit(i) => update_mask_size(&i.dirty_mask),
+                    UpdateMask::Player(i) => update_mask_size(&i.dirty_mask),
+                    UpdateMask::GameObject(i) => update_mask_size(&i.dirty_mask),
+                    UpdateMask::DynamicObject(i) => update_mask_size(&i.dirty_mask),
+                    UpdateMask::Corpse(i) => update_mask_size(&i.dirty_mask),
+                }
+            }
+        }
+    };
+}
+
 pub(crate) use update_item;
+pub(crate) use update_mask;
--- wow_world_messages/src/helper/wrath/update_mask/mod.rs
+++ wow_world_messages/src/helper/wrath/update_mask/mod.rs
@@ -4,7 +4,8 @@ pub use impls::*;
 
 use crate::helper::update_mask_common;
 use crate::helper::update_mask_common::{
-    update_item, update_mask_size, CONTAINER, CORPSE, DYNAMICOBJECT, GAMEOBJECT, ITEM, PLAYER, UNIT,
+    update_item, update_mask, update_mask_size, CONTAINER, CORPSE, DYNAMICOBJECT, GAMEOBJECT, ITEM,
+    PLAYER, UNIT,
 };
 use std::collections::BTreeMap;
 use std::io;
@@ -22,80 +23,4 @@ update_item!(
 );
 update_item!(UpdateCorpse, UpdateCorpseBuilder, CORPSE);
 
-#[derive(Debug, Hash, Clone, PartialEq, Eq)]
-pub enum UpdateMask {
-    Item(UpdateItem),
-    Container(UpdateContainer),
-    Unit(UpdateUnit),
-    Player(UpdatePlayer),
-    GameObject(UpdateGameObject),
-    DynamicObject(UpdateDynamicObject),
-    Corpse(UpdateCorpse),
-}
-
-impl Default for UpdateMask {
-    fn default() -> Self {
-        Self::Item(Default::default())
-    }
-}
-
-impl UpdateMask {
-    pub(crate) fn read(r: &mut impl Read) -> Result<Self, io::Error> {
-        let (header, values) = update_mask_common::read_inner(r)?;
-
-        let ty = match values.get(&2) {
-            None => {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    "Missing object TYPE",
-                ))
-            }
-            Some(ty) => *ty,
-        };
-
-        Ok(if (ty & CONTAINER) != 0 {
-            Self::Container(UpdateContainer::from_inners(header, values))
-        } else if (ty & ITEM) != 0 {
-            Self::Item(UpdateItem::from_inners(header, values))
-        } else if (ty & PLAYER) != 0 {
-            Self::Player(UpdatePlayer::from_inners(header, values))
-        } else if (ty & UNIT) != 0 {
-            Self::Unit(UpdateUnit::from_inners(header, values))
-        } else if (ty & GAMEOBJECT) != 0 {
-            Self::GameObject(UpdateGameObject::from_inners(header, values))
-        } else if (ty & DYNAMICOBJECT) != 0 {
-            Self::DynamicObject(UpdateDynamicObject::from_inners(header, values))
-        } else if (ty & CORPSE) != 0 {
-            Self::Corpse(UpdateCorpse::from_inners(header, values))
-        } else {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Object type not valid",
-            ));
-        })
-    }
-
-    pub(crate) fn write_into_vec(&self, v: &mut Vec<u8>) -> Result<(), std::io::Error> {
-        match self {
-            UpdateMask::Item(i) => i.write_into_vec(v),
-            UpdateMask::Container(i) => i.write_into_vec(v),
-            UpdateMask::Unit(i) => i.write_into_vec(v),
-            UpdateMask::Player(i) => i.write_into_vec(v),
-            UpdateMask::GameObject(i) => i.write_into_vec(v),
-            UpdateMask::DynamicObject(i) => i.write_into_vec(v),
-            UpdateMask::Corpse(i) => i.write_into_vec(v),
-        }
-    }
-
-    pub(crate) fn size(&self) -> usize {
-        match self {
-            UpdateMask::Item(i) => update_mask_size(&i.dirty_mask),
-            UpdateMask::Container(i) => update_mask_size(&i.dirty_mask),
-            UpdateMask::Unit(i) => update_mask_size(&i.dirty_mask),
-            UpdateMask::Player(i) => update_mask_size(&i.dirty_mask),
-            UpdateMask::GameObject(i) => update_mask_size(&i.dirty_mask),
-            UpdateMask::DynamicObject(i) => update_mask_size(&i.dirty_mask),
-            UpdateMask::Corpse(i) => update_mask_size(&i.dirty_mask),
-        }
-    }
-}
+update_mask!();
