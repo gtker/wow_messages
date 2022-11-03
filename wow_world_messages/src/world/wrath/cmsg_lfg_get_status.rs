bb028ed14c5360a850fedb110d524e9394c0ab56
biggle <bigglessss@proton.me>
2022-11-03 20:35:40 +0000

    Add CMSG_LFG_GET_STATUS for Wrath

---
+++ wow_world_messages/src/world/wrath/cmsg_lfg_get_status.rs
@@ -0,0 +1,35 @@
+use std::convert::{TryFrom, TryInto};
+use std::io::{Write, Read};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+/// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/lfg/cmsg_lfg_get_status.wowm:3`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/lfg/cmsg_lfg_get_status.wowm#L3):
+/// ```text
+/// cmsg CMSG_LFG_GET_STATUS = 0x0296 {
+/// }
+/// ```
+pub struct CMSG_LFG_GET_STATUS {
+}
+
+impl crate::Message for CMSG_LFG_GET_STATUS {
+    const OPCODE: u32 = 0x0296;
+
+    fn size_without_header(&self) -> u32 {
+        0
+    }
+
+    fn write_into_vec(&self, w: &mut Vec<u8>) -> Result<(), std::io::Error> {
+        Ok(())
+    }
+    fn read_body(r: &mut &[u8], body_size: u32) -> std::result::Result<Self, crate::errors::ParseError> {
+        if body_size != 0 {
+            return Err(crate::errors::ParseError::InvalidSize { opcode: 0x0296, size: body_size as u32 });
+        }
+
+        Ok(Self {
+        })
+    }
+
+}
+#[cfg(feature = "wrath")]
+impl crate::world::wrath::ClientMessage for CMSG_LFG_GET_STATUS {}
+
