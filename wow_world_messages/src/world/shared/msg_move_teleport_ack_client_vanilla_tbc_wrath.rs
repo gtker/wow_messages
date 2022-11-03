475448e024baa63343364c6910badfd2805d803e
biggle <bigglessss@proton.me>
2022-11-03 19:25:48 +0000

    Change Guid -> PackedGuid in MSG_MOVE_TELEPORT_ACK_Client

--- wow_world_messages/src/world/shared/msg_move_teleport_ack_client_vanilla_tbc_wrath.rs
+++ wow_world_messages/src/world/shared/msg_move_teleport_ack_client_vanilla_tbc_wrath.rs
@@ -2,13 +2,13 @@ use std::convert::{TryFrom, TryInto};
 use crate::Guid;
 use std::io::{Write, Read};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
 /// Response to [`MSG_MOVE_TELEPORT_ACK_Server`](crate::world::vanilla::MSG_MOVE_TELEPORT_ACK_Server), at which point [`MSG_MOVE_TELEPORT_ACK_Server`](crate::world::vanilla::MSG_MOVE_TELEPORT_ACK_Server) should be sent to observing players.
 ///
 /// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/movement/msg/msg_move_teleport_ack.wowm:1`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/movement/msg/msg_move_teleport_ack.wowm#L1):
 /// ```text
 /// cmsg MSG_MOVE_TELEPORT_ACK_Client = 0x00C7 {
-///     Guid guid;
+///     PackedGuid guid;
 ///     u32 movement_counter;
 ///     u32 time_in_msecs;
 /// }
@@ -23,12 +23,13 @@ impl crate::Message for MSG_MOVE_TELEPORT_ACK_Client {
     const OPCODE: u32 = 0x00c7;
 
     fn size_without_header(&self) -> u32 {
-        16
+        self.size() as u32
     }
 
     fn write_into_vec(&self, w: &mut Vec<u8>) -> Result<(), std::io::Error> {
-        // guid: Guid
-        w.write_all(&self.guid.guid().to_le_bytes())?;
+        let size_assert_header_size = w.len();
+        // guid: PackedGuid
+        self.guid.write_packed_guid_into_vec(w);
 
         // movement_counter: u32
         w.write_all(&self.movement_counter.to_le_bytes())?;
@@ -36,15 +37,12 @@ impl crate::Message for MSG_MOVE_TELEPORT_ACK_Client {
         // time_in_msecs: u32
         w.write_all(&self.time_in_msecs.to_le_bytes())?;
 
+        assert_eq!(self.size() as usize + size_assert_header_size, w.len(), "Mismatch in pre-calculated size and actual written size. This needs investigation as it will cause problems in the game client when sent");
         Ok(())
     }
     fn read_body(r: &mut &[u8], body_size: u32) -> std::result::Result<Self, crate::errors::ParseError> {
-        if body_size != 16 {
-            return Err(crate::errors::ParseError::InvalidSize { opcode: 0x00C7, size: body_size as u32 });
-        }
-
-        // guid: Guid
-        let guid = Guid::read(r)?;
+        // guid: PackedGuid
+        let guid = Guid::read_packed(r)?;
 
         // movement_counter: u32
         let movement_counter = crate::util::read_u32_le(r)?;
@@ -68,4 +66,12 @@ impl crate::world::tbc::ClientMessage for MSG_MOVE_TELEPORT_ACK_Client {}
 
 #[cfg(feature = "wrath")]
 impl crate::world::wrath::ClientMessage for MSG_MOVE_TELEPORT_ACK_Client {}
+
+impl MSG_MOVE_TELEPORT_ACK_Client {
+    pub(crate) fn size(&self) -> usize {
+        self.guid.size() // guid: Guid
+        + 4 // movement_counter: u32
+        + 4 // time_in_msecs: u32
+    }
+}
 
