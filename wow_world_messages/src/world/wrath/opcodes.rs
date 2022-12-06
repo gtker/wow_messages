742139880e375e15841fa81b6246979ce34880b2
Gtker <git@gtker.com>
2022-12-06 14:55:32 +0100

    Add SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT for Wrath

--- wow_world_messages/src/world/wrath/opcodes.rs
+++ wow_world_messages/src/world/wrath/opcodes.rs
@@ -7421,6 +7421,7 @@ use crate::world::wrath::SMSG_GMTICKET_UPDATETEXT;
 use crate::world::wrath::SMSG_ACCOUNT_DATA_TIMES;
 use crate::world::wrath::SMSG_UPDATE_ACCOUNT_DATA;
 use crate::world::wrath::SMSG_GMTICKET_GETTICKET;
+use crate::world::wrath::SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT;
 use crate::world::wrath::SMSG_GAMEOBJECT_DESPAWN_ANIM;
 use crate::world::wrath::MSG_CORPSE_QUERY_Server;
 use crate::world::wrath::SMSG_GMTICKET_DELETETICKET;
@@ -7812,6 +7813,7 @@ pub enum ServerOpcodeMessage {
     SMSG_ACCOUNT_DATA_TIMES(SMSG_ACCOUNT_DATA_TIMES),
     SMSG_UPDATE_ACCOUNT_DATA(SMSG_UPDATE_ACCOUNT_DATA),
     SMSG_GMTICKET_GETTICKET(SMSG_GMTICKET_GETTICKET),
+    SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT(SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT),
     SMSG_GAMEOBJECT_DESPAWN_ANIM(SMSG_GAMEOBJECT_DESPAWN_ANIM),
     MSG_CORPSE_QUERY(MSG_CORPSE_QUERY_Server),
     SMSG_GMTICKET_DELETETICKET(SMSG_GMTICKET_DELETETICKET),
@@ -8205,6 +8207,7 @@ impl ServerOpcodeMessage {
             0x0209 => Ok(Self::SMSG_ACCOUNT_DATA_TIMES(<SMSG_ACCOUNT_DATA_TIMES as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0209, size: body_size, io, } } else { a } })?)),
             0x020C => Ok(Self::SMSG_UPDATE_ACCOUNT_DATA(<SMSG_UPDATE_ACCOUNT_DATA as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x020C, size: body_size, io, } } else { a } })?)),
             0x0212 => Ok(Self::SMSG_GMTICKET_GETTICKET(<SMSG_GMTICKET_GETTICKET as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0212, size: body_size, io, } } else { a } })?)),
+            0x0214 => Ok(Self::SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT(<SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0214, size: body_size, io, } } else { a } })?)),
             0x0215 => Ok(Self::SMSG_GAMEOBJECT_DESPAWN_ANIM(<SMSG_GAMEOBJECT_DESPAWN_ANIM as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0215, size: body_size, io, } } else { a } })?)),
             0x0216 => Ok(Self::MSG_CORPSE_QUERY(<MSG_CORPSE_QUERY_Server as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0216, size: body_size, io, } } else { a } })?)),
             0x0218 => Ok(Self::SMSG_GMTICKET_DELETETICKET(<SMSG_GMTICKET_DELETETICKET as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0218, size: body_size, io, } } else { a } })?)),
@@ -8747,6 +8750,7 @@ impl ServerOpcodeMessage {
             Self::SMSG_ACCOUNT_DATA_TIMES(c) => c.write_encrypted_server(w, e),
             Self::SMSG_UPDATE_ACCOUNT_DATA(c) => c.write_encrypted_server(w, e),
             Self::SMSG_GMTICKET_GETTICKET(c) => c.write_encrypted_server(w, e),
+            Self::SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT(c) => c.write_encrypted_server(w, e),
             Self::SMSG_GAMEOBJECT_DESPAWN_ANIM(c) => c.write_encrypted_server(w, e),
             Self::MSG_CORPSE_QUERY(c) => c.write_encrypted_server(w, e),
             Self::SMSG_GMTICKET_DELETETICKET(c) => c.write_encrypted_server(w, e),
@@ -9141,6 +9145,7 @@ impl ServerOpcodeMessage {
             Self::SMSG_ACCOUNT_DATA_TIMES(c) => c.write_unencrypted_server(w),
             Self::SMSG_UPDATE_ACCOUNT_DATA(c) => c.write_unencrypted_server(w),
             Self::SMSG_GMTICKET_GETTICKET(c) => c.write_unencrypted_server(w),
+            Self::SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT(c) => c.write_unencrypted_server(w),
             Self::SMSG_GAMEOBJECT_DESPAWN_ANIM(c) => c.write_unencrypted_server(w),
             Self::MSG_CORPSE_QUERY(c) => c.write_unencrypted_server(w),
             Self::SMSG_GMTICKET_DELETETICKET(c) => c.write_unencrypted_server(w),
@@ -9535,6 +9540,7 @@ impl ServerOpcodeMessage {
             Self::SMSG_ACCOUNT_DATA_TIMES(c) => c.tokio_write_encrypted_server(w, e).await,
             Self::SMSG_UPDATE_ACCOUNT_DATA(c) => c.tokio_write_encrypted_server(w, e).await,
             Self::SMSG_GMTICKET_GETTICKET(c) => c.tokio_write_encrypted_server(w, e).await,
+            Self::SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT(c) => c.tokio_write_encrypted_server(w, e).await,
             Self::SMSG_GAMEOBJECT_DESPAWN_ANIM(c) => c.tokio_write_encrypted_server(w, e).await,
             Self::MSG_CORPSE_QUERY(c) => c.tokio_write_encrypted_server(w, e).await,
             Self::SMSG_GMTICKET_DELETETICKET(c) => c.tokio_write_encrypted_server(w, e).await,
@@ -9929,6 +9935,7 @@ impl ServerOpcodeMessage {
             Self::SMSG_ACCOUNT_DATA_TIMES(c) => c.tokio_write_unencrypted_server(w).await,
             Self::SMSG_UPDATE_ACCOUNT_DATA(c) => c.tokio_write_unencrypted_server(w).await,
             Self::SMSG_GMTICKET_GETTICKET(c) => c.tokio_write_unencrypted_server(w).await,
+            Self::SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT(c) => c.tokio_write_unencrypted_server(w).await,
             Self::SMSG_GAMEOBJECT_DESPAWN_ANIM(c) => c.tokio_write_unencrypted_server(w).await,
             Self::MSG_CORPSE_QUERY(c) => c.tokio_write_unencrypted_server(w).await,
             Self::SMSG_GMTICKET_DELETETICKET(c) => c.tokio_write_unencrypted_server(w).await,
@@ -10323,6 +10330,7 @@ impl ServerOpcodeMessage {
             Self::SMSG_ACCOUNT_DATA_TIMES(c) => c.astd_write_encrypted_server(w, e).await,
             Self::SMSG_UPDATE_ACCOUNT_DATA(c) => c.astd_write_encrypted_server(w, e).await,
             Self::SMSG_GMTICKET_GETTICKET(c) => c.astd_write_encrypted_server(w, e).await,
+            Self::SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT(c) => c.astd_write_encrypted_server(w, e).await,
             Self::SMSG_GAMEOBJECT_DESPAWN_ANIM(c) => c.astd_write_encrypted_server(w, e).await,
             Self::MSG_CORPSE_QUERY(c) => c.astd_write_encrypted_server(w, e).await,
             Self::SMSG_GMTICKET_DELETETICKET(c) => c.astd_write_encrypted_server(w, e).await,
@@ -10717,6 +10725,7 @@ impl ServerOpcodeMessage {
             Self::SMSG_ACCOUNT_DATA_TIMES(c) => c.astd_write_unencrypted_server(w).await,
             Self::SMSG_UPDATE_ACCOUNT_DATA(c) => c.astd_write_unencrypted_server(w).await,
             Self::SMSG_GMTICKET_GETTICKET(c) => c.astd_write_unencrypted_server(w).await,
+            Self::SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT(c) => c.astd_write_unencrypted_server(w).await,
             Self::SMSG_GAMEOBJECT_DESPAWN_ANIM(c) => c.astd_write_unencrypted_server(w).await,
             Self::MSG_CORPSE_QUERY(c) => c.astd_write_unencrypted_server(w).await,
             Self::SMSG_GMTICKET_DELETETICKET(c) => c.astd_write_unencrypted_server(w).await,
@@ -11121,6 +11130,7 @@ impl std::fmt::Display for ServerOpcodeMessage {
             ServerOpcodeMessage::SMSG_ACCOUNT_DATA_TIMES(_) => "SMSG_ACCOUNT_DATA_TIMES",
             ServerOpcodeMessage::SMSG_UPDATE_ACCOUNT_DATA(_) => "SMSG_UPDATE_ACCOUNT_DATA",
             ServerOpcodeMessage::SMSG_GMTICKET_GETTICKET(_) => "SMSG_GMTICKET_GETTICKET",
+            ServerOpcodeMessage::SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT(_) => "SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT",
             ServerOpcodeMessage::SMSG_GAMEOBJECT_DESPAWN_ANIM(_) => "SMSG_GAMEOBJECT_DESPAWN_ANIM",
             ServerOpcodeMessage::MSG_CORPSE_QUERY(_) => "MSG_CORPSE_QUERY_Server",
             ServerOpcodeMessage::SMSG_GMTICKET_DELETETICKET(_) => "SMSG_GMTICKET_DELETETICKET",
@@ -12645,6 +12655,12 @@ impl From<SMSG_UPDATE_ACCOUNT_DATA> for ServerOpcodeMessage {
 impl From<SMSG_GMTICKET_GETTICKET> for ServerOpcodeMessage {
     fn from(c: SMSG_GMTICKET_GETTICKET) -> Self {
         Self::SMSG_GMTICKET_GETTICKET(c)
+    }
+}
+
+impl From<SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT> for ServerOpcodeMessage {
+    fn from(c: SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT) -> Self {
+        Self::SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT(c)
     }
 }
 
---
+++ wow_world_messages/src/world/wrath/smsg_update_instance_encounter_unit.rs
@@ -0,0 +1,327 @@
+use std::convert::{TryFrom, TryInto};
+use crate::Guid;
+use crate::world::wrath::EncounterFrame;
+use std::io::{Write, Read};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
+/// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/_need_sorting/smsg_update_instance_encounter_unit.wowm:16`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/_need_sorting/smsg_update_instance_encounter_unit.wowm#L16):
+/// ```text
+/// smsg SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT = 0x0214 {
+///     EncounterFrame frame;
+///     if (frame == ENGAGE
+///         || frame == DISENGAGE
+///         || frame == UPDATE_PRIORITY) {
+///         PackedGuid guid;
+///         u8 parameter1;
+///     }
+///     else if (frame == ADD_TIMER
+///         || frame == ENABLE_OBJECTIVE
+///         || frame == DISABLE_OBJECTIVE) {
+///         u8 parameter2;
+///     }
+///     else if (frame == UPDATE_OBJECTIVE) {
+///         u8 parameter3;
+///         u8 parameter4;
+///     }
+/// }
+/// ```
+pub struct SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT {
+    pub frame: SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame,
+}
+
+impl crate::Message for SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT {
+    const OPCODE: u32 = 0x0214;
+
+    fn size_without_header(&self) -> u32 {
+        self.size() as u32
+    }
+
+    fn write_into_vec(&self, w: &mut Vec<u8>) -> Result<(), std::io::Error> {
+        let size_assert_header_size = w.len();
+        // frame: EncounterFrame
+        w.write_all(&(self.frame.as_int() as u32).to_le_bytes())?;
+
+        match &self.frame {
+            SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::Engage {
+                guid,
+                parameter1,
+            } => {
+                // guid: PackedGuid
+                guid.write_packed_guid_into_vec(w);
+
+                // parameter1: u8
+                w.write_all(&parameter1.to_le_bytes())?;
+
+            }
+            SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::Disengage {
+                guid,
+                parameter1,
+            } => {
+                // guid: PackedGuid
+                guid.write_packed_guid_into_vec(w);
+
+                // parameter1: u8
+                w.write_all(&parameter1.to_le_bytes())?;
+
+            }
+            SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::UpdatePriority {
+                guid,
+                parameter1,
+            } => {
+                // guid: PackedGuid
+                guid.write_packed_guid_into_vec(w);
+
+                // parameter1: u8
+                w.write_all(&parameter1.to_le_bytes())?;
+
+            }
+            SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::AddTimer {
+                parameter2,
+            } => {
+                // parameter2: u8
+                w.write_all(&parameter2.to_le_bytes())?;
+
+            }
+            SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::EnableObjective {
+                parameter2,
+            } => {
+                // parameter2: u8
+                w.write_all(&parameter2.to_le_bytes())?;
+
+            }
+            SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::UpdateObjective {
+                parameter3,
+                parameter4,
+            } => {
+                // parameter3: u8
+                w.write_all(&parameter3.to_le_bytes())?;
+
+                // parameter4: u8
+                w.write_all(&parameter4.to_le_bytes())?;
+
+            }
+            SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::DisableObjective {
+                parameter2,
+            } => {
+                // parameter2: u8
+                w.write_all(&parameter2.to_le_bytes())?;
+
+            }
+            SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::RefreshFrames => {}
+        }
+
+        assert_eq!(self.size() as usize + size_assert_header_size, w.len(), "Mismatch in pre-calculated size and actual written size. This needs investigation as it will cause problems in the game client when sent");
+        Ok(())
+    }
+    fn read_body(r: &mut &[u8], body_size: u32) -> std::result::Result<Self, crate::errors::ParseError> {
+        if !(4..=14).contains(&body_size) {
+            return Err(crate::errors::ParseError::InvalidSize { opcode: 0x0214, size: body_size as u32 });
+        }
+
+        // frame: EncounterFrame
+        let frame: EncounterFrame = crate::util::read_u32_le(r)?.try_into()?;
+
+        let frame_if = match frame {
+            EncounterFrame::Engage => {
+                // guid: PackedGuid
+                let guid = Guid::read_packed(r)?;
+
+                // parameter1: u8
+                let parameter1 = crate::util::read_u8_le(r)?;
+
+                SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::Engage {
+                    guid,
+                    parameter1,
+                }
+            }
+            EncounterFrame::Disengage => {
+                // guid: PackedGuid
+                let guid = Guid::read_packed(r)?;
+
+                // parameter1: u8
+                let parameter1 = crate::util::read_u8_le(r)?;
+
+                SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::Disengage {
+                    guid,
+                    parameter1,
+                }
+            }
+            EncounterFrame::UpdatePriority => {
+                // guid: PackedGuid
+                let guid = Guid::read_packed(r)?;
+
+                // parameter1: u8
+                let parameter1 = crate::util::read_u8_le(r)?;
+
+                SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::UpdatePriority {
+                    guid,
+                    parameter1,
+                }
+            }
+            EncounterFrame::AddTimer => {
+                // parameter2: u8
+                let parameter2 = crate::util::read_u8_le(r)?;
+
+                SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::AddTimer {
+                    parameter2,
+                }
+            }
+            EncounterFrame::EnableObjective => {
+                // parameter2: u8
+                let parameter2 = crate::util::read_u8_le(r)?;
+
+                SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::EnableObjective {
+                    parameter2,
+                }
+            }
+            EncounterFrame::UpdateObjective => {
+                // parameter3: u8
+                let parameter3 = crate::util::read_u8_le(r)?;
+
+                // parameter4: u8
+                let parameter4 = crate::util::read_u8_le(r)?;
+
+                SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::UpdateObjective {
+                    parameter3,
+                    parameter4,
+                }
+            }
+            EncounterFrame::DisableObjective => {
+                // parameter2: u8
+                let parameter2 = crate::util::read_u8_le(r)?;
+
+                SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::DisableObjective {
+                    parameter2,
+                }
+            }
+            EncounterFrame::RefreshFrames => SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame::RefreshFrames,
+        };
+
+        Ok(Self {
+            frame: frame_if,
+        })
+    }
+
+}
+#[cfg(feature = "wrath")]
+impl crate::world::wrath::ServerMessage for SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT {}
+
+impl SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT {
+    pub(crate) fn size(&self) -> usize {
+        self.frame.size() // frame: SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame {
+    Engage {
+        guid: Guid,
+        parameter1: u8,
+    },
+    Disengage {
+        guid: Guid,
+        parameter1: u8,
+    },
+    UpdatePriority {
+        guid: Guid,
+        parameter1: u8,
+    },
+    AddTimer {
+        parameter2: u8,
+    },
+    EnableObjective {
+        parameter2: u8,
+    },
+    UpdateObjective {
+        parameter3: u8,
+        parameter4: u8,
+    },
+    DisableObjective {
+        parameter2: u8,
+    },
+    RefreshFrames,
+}
+
+impl Default for SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame {
+    fn default() -> Self {
+        // First enumerator without any fields
+        Self::RefreshFrames
+    }
+}
+
+impl SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame {
+    pub(crate) const fn as_int(&self) -> u32 {
+        match self {
+            Self::Engage { .. } => 0,
+            Self::Disengage { .. } => 1,
+            Self::UpdatePriority { .. } => 2,
+            Self::AddTimer { .. } => 3,
+            Self::EnableObjective { .. } => 4,
+            Self::UpdateObjective { .. } => 5,
+            Self::DisableObjective { .. } => 6,
+            Self::RefreshFrames => 7,
+        }
+    }
+
+}
+
+impl SMSG_UPDATE_INSTANCE_ENCOUNTER_UNIT_EncounterFrame {
+    pub(crate) fn size(&self) -> usize {
+        match self {
+            Self::Engage {
+                guid,
+                parameter1,
+            } => {
+                4
+                + guid.size() // guid: Guid
+                + 1 // parameter1: u8
+            }
+            Self::Disengage {
+                guid,
+                parameter1,
+            } => {
+                4
+                + guid.size() // guid: Guid
+                + 1 // parameter1: u8
+            }
+            Self::UpdatePriority {
+                guid,
+                parameter1,
+            } => {
+                4
+                + guid.size() // guid: Guid
+                + 1 // parameter1: u8
+            }
+            Self::AddTimer {
+                parameter2,
+            } => {
+                4
+                + 1 // parameter2: u8
+            }
+            Self::EnableObjective {
+                parameter2,
+            } => {
+                4
+                + 1 // parameter2: u8
+            }
+            Self::UpdateObjective {
+                parameter3,
+                parameter4,
+            } => {
+                4
+                + 1 // parameter3: u8
+                + 1 // parameter4: u8
+            }
+            Self::DisableObjective {
+                parameter2,
+            } => {
+                4
+                + 1 // parameter2: u8
+            }
+            Self::RefreshFrames => {
+                4
+            }
+        }
+    }
+}
+
