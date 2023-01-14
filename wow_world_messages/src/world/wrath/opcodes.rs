ff7ada145c06aa34745e9256cacdff4058fadcdb
Gtker <git@gtker.com>
2023-01-14 12:38:18 +0100

    Add SMSG_LFG_UPDATE_PLAYER for Wrath

--- wow_world_messages/src/world/wrath/opcodes.rs
+++ wow_world_messages/src/world/wrath/opcodes.rs
@@ -7559,6 +7559,7 @@ use crate::world::wrath::SMSG_LFG_PROPOSAL_UPDATE;
 use crate::world::wrath::SMSG_LFG_ROLE_CHECK_UPDATE;
 use crate::world::wrath::SMSG_LFG_JOIN_RESULT;
 use crate::world::wrath::SMSG_LFG_QUEUE_STATUS;
+use crate::world::wrath::SMSG_LFG_UPDATE_PLAYER;
 use crate::world::wrath::SMSG_TITLE_EARNED;
 use crate::world::wrath::SMSG_ARENA_ERROR;
 use crate::world::wrath::MSG_INSPECT_ARENA_TEAMS_Server;
@@ -7959,6 +7960,7 @@ pub enum ServerOpcodeMessage {
     SMSG_LFG_ROLE_CHECK_UPDATE(SMSG_LFG_ROLE_CHECK_UPDATE),
     SMSG_LFG_JOIN_RESULT(SMSG_LFG_JOIN_RESULT),
     SMSG_LFG_QUEUE_STATUS(SMSG_LFG_QUEUE_STATUS),
+    SMSG_LFG_UPDATE_PLAYER(SMSG_LFG_UPDATE_PLAYER),
     SMSG_TITLE_EARNED(SMSG_TITLE_EARNED),
     SMSG_ARENA_ERROR(SMSG_ARENA_ERROR),
     MSG_INSPECT_ARENA_TEAMS(MSG_INSPECT_ARENA_TEAMS_Server),
@@ -8361,6 +8363,7 @@ impl ServerOpcodeMessage {
             0x0363 => Ok(Self::SMSG_LFG_ROLE_CHECK_UPDATE(<SMSG_LFG_ROLE_CHECK_UPDATE as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0363, size: body_size, io, } } else { a } })?)),
             0x0364 => Ok(Self::SMSG_LFG_JOIN_RESULT(<SMSG_LFG_JOIN_RESULT as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0364, size: body_size, io, } } else { a } })?)),
             0x0365 => Ok(Self::SMSG_LFG_QUEUE_STATUS(<SMSG_LFG_QUEUE_STATUS as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0365, size: body_size, io, } } else { a } })?)),
+            0x0367 => Ok(Self::SMSG_LFG_UPDATE_PLAYER(<SMSG_LFG_UPDATE_PLAYER as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0367, size: body_size, io, } } else { a } })?)),
             0x0373 => Ok(Self::SMSG_TITLE_EARNED(<SMSG_TITLE_EARNED as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0373, size: body_size, io, } } else { a } })?)),
             0x0376 => Ok(Self::SMSG_ARENA_ERROR(<SMSG_ARENA_ERROR as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0376, size: body_size, io, } } else { a } })?)),
             0x0377 => Ok(Self::MSG_INSPECT_ARENA_TEAMS(<MSG_INSPECT_ARENA_TEAMS_Server as crate::Message>::read_body(&mut r, body_size).map_err(|a| { if let ParseError::Io(io) = a { ParseError::BufferSizeTooSmall { opcode: 0x0377, size: body_size, io, } } else { a } })?)),
@@ -8912,6 +8915,7 @@ impl ServerOpcodeMessage {
             Self::SMSG_LFG_ROLE_CHECK_UPDATE(c) => c.write_encrypted_server(w, e),
             Self::SMSG_LFG_JOIN_RESULT(c) => c.write_encrypted_server(w, e),
             Self::SMSG_LFG_QUEUE_STATUS(c) => c.write_encrypted_server(w, e),
+            Self::SMSG_LFG_UPDATE_PLAYER(c) => c.write_encrypted_server(w, e),
             Self::SMSG_TITLE_EARNED(c) => c.write_encrypted_server(w, e),
             Self::SMSG_ARENA_ERROR(c) => c.write_encrypted_server(w, e),
             Self::MSG_INSPECT_ARENA_TEAMS(c) => c.write_encrypted_server(w, e),
@@ -9315,6 +9319,7 @@ impl ServerOpcodeMessage {
             Self::SMSG_LFG_ROLE_CHECK_UPDATE(c) => c.write_unencrypted_server(w),
             Self::SMSG_LFG_JOIN_RESULT(c) => c.write_unencrypted_server(w),
             Self::SMSG_LFG_QUEUE_STATUS(c) => c.write_unencrypted_server(w),
+            Self::SMSG_LFG_UPDATE_PLAYER(c) => c.write_unencrypted_server(w),
             Self::SMSG_TITLE_EARNED(c) => c.write_unencrypted_server(w),
             Self::SMSG_ARENA_ERROR(c) => c.write_unencrypted_server(w),
             Self::MSG_INSPECT_ARENA_TEAMS(c) => c.write_unencrypted_server(w),
@@ -9718,6 +9723,7 @@ impl ServerOpcodeMessage {
             Self::SMSG_LFG_ROLE_CHECK_UPDATE(c) => c.tokio_write_encrypted_server(w, e).await,
             Self::SMSG_LFG_JOIN_RESULT(c) => c.tokio_write_encrypted_server(w, e).await,
             Self::SMSG_LFG_QUEUE_STATUS(c) => c.tokio_write_encrypted_server(w, e).await,
+            Self::SMSG_LFG_UPDATE_PLAYER(c) => c.tokio_write_encrypted_server(w, e).await,
             Self::SMSG_TITLE_EARNED(c) => c.tokio_write_encrypted_server(w, e).await,
             Self::SMSG_ARENA_ERROR(c) => c.tokio_write_encrypted_server(w, e).await,
             Self::MSG_INSPECT_ARENA_TEAMS(c) => c.tokio_write_encrypted_server(w, e).await,
@@ -10121,6 +10127,7 @@ impl ServerOpcodeMessage {
             Self::SMSG_LFG_ROLE_CHECK_UPDATE(c) => c.tokio_write_unencrypted_server(w).await,
             Self::SMSG_LFG_JOIN_RESULT(c) => c.tokio_write_unencrypted_server(w).await,
             Self::SMSG_LFG_QUEUE_STATUS(c) => c.tokio_write_unencrypted_server(w).await,
+            Self::SMSG_LFG_UPDATE_PLAYER(c) => c.tokio_write_unencrypted_server(w).await,
             Self::SMSG_TITLE_EARNED(c) => c.tokio_write_unencrypted_server(w).await,
             Self::SMSG_ARENA_ERROR(c) => c.tokio_write_unencrypted_server(w).await,
             Self::MSG_INSPECT_ARENA_TEAMS(c) => c.tokio_write_unencrypted_server(w).await,
@@ -10524,6 +10531,7 @@ impl ServerOpcodeMessage {
             Self::SMSG_LFG_ROLE_CHECK_UPDATE(c) => c.astd_write_encrypted_server(w, e).await,
             Self::SMSG_LFG_JOIN_RESULT(c) => c.astd_write_encrypted_server(w, e).await,
             Self::SMSG_LFG_QUEUE_STATUS(c) => c.astd_write_encrypted_server(w, e).await,
+            Self::SMSG_LFG_UPDATE_PLAYER(c) => c.astd_write_encrypted_server(w, e).await,
             Self::SMSG_TITLE_EARNED(c) => c.astd_write_encrypted_server(w, e).await,
             Self::SMSG_ARENA_ERROR(c) => c.astd_write_encrypted_server(w, e).await,
             Self::MSG_INSPECT_ARENA_TEAMS(c) => c.astd_write_encrypted_server(w, e).await,
@@ -10927,6 +10935,7 @@ impl ServerOpcodeMessage {
             Self::SMSG_LFG_ROLE_CHECK_UPDATE(c) => c.astd_write_unencrypted_server(w).await,
             Self::SMSG_LFG_JOIN_RESULT(c) => c.astd_write_unencrypted_server(w).await,
             Self::SMSG_LFG_QUEUE_STATUS(c) => c.astd_write_unencrypted_server(w).await,
+            Self::SMSG_LFG_UPDATE_PLAYER(c) => c.astd_write_unencrypted_server(w).await,
             Self::SMSG_TITLE_EARNED(c) => c.astd_write_unencrypted_server(w).await,
             Self::SMSG_ARENA_ERROR(c) => c.astd_write_unencrypted_server(w).await,
             Self::MSG_INSPECT_ARENA_TEAMS(c) => c.astd_write_unencrypted_server(w).await,
@@ -11340,6 +11349,7 @@ impl std::fmt::Display for ServerOpcodeMessage {
             ServerOpcodeMessage::SMSG_LFG_ROLE_CHECK_UPDATE(_) => "SMSG_LFG_ROLE_CHECK_UPDATE",
             ServerOpcodeMessage::SMSG_LFG_JOIN_RESULT(_) => "SMSG_LFG_JOIN_RESULT",
             ServerOpcodeMessage::SMSG_LFG_QUEUE_STATUS(_) => "SMSG_LFG_QUEUE_STATUS",
+            ServerOpcodeMessage::SMSG_LFG_UPDATE_PLAYER(_) => "SMSG_LFG_UPDATE_PLAYER",
             ServerOpcodeMessage::SMSG_TITLE_EARNED(_) => "SMSG_TITLE_EARNED",
             ServerOpcodeMessage::SMSG_ARENA_ERROR(_) => "SMSG_ARENA_ERROR",
             ServerOpcodeMessage::MSG_INSPECT_ARENA_TEAMS(_) => "MSG_INSPECT_ARENA_TEAMS_Server",
@@ -13563,6 +13573,12 @@ impl From<SMSG_LFG_JOIN_RESULT> for ServerOpcodeMessage {
 impl From<SMSG_LFG_QUEUE_STATUS> for ServerOpcodeMessage {
     fn from(c: SMSG_LFG_QUEUE_STATUS) -> Self {
         Self::SMSG_LFG_QUEUE_STATUS(c)
+    }
+}
+
+impl From<SMSG_LFG_UPDATE_PLAYER> for ServerOpcodeMessage {
+    fn from(c: SMSG_LFG_UPDATE_PLAYER) -> Self {
+        Self::SMSG_LFG_UPDATE_PLAYER(c)
     }
 }
 
---
+++ wow_world_messages/src/world/wrath/smsg_lfg_update_player.rs
@@ -0,0 +1,197 @@
+use std::convert::{TryFrom, TryInto};
+use crate::world::wrath::LfgJoinStatus;
+use crate::world::wrath::LfgUpdateType;
+use std::io::{Write, Read};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
+/// Auto generated from the original `wowm` in file [`wow_message_parser/wowm/world/_need_sorting/smsg_lfg_update_player.wowm:27`](https://github.com/gtker/wow_messages/tree/main/wow_message_parser/wowm/world/_need_sorting/smsg_lfg_update_player.wowm#L27):
+/// ```text
+/// smsg SMSG_LFG_UPDATE_PLAYER = 0x0367 {
+///     LfgUpdateType update_type;
+///     LfgJoinStatus join_status;
+///     if (join_status == JOINED) {
+///         u8 queued;
+///         u8 no_partial_clear;
+///         u8 achievements;
+///         u8 amount_of_dungeons;
+///         u32[amount_of_dungeons] dungeons;
+///         CString comment;
+///     }
+/// }
+/// ```
+pub struct SMSG_LFG_UPDATE_PLAYER {
+    pub update_type: LfgUpdateType,
+    pub join_status: SMSG_LFG_UPDATE_PLAYER_LfgJoinStatus,
+}
+
+impl crate::Message for SMSG_LFG_UPDATE_PLAYER {
+    const OPCODE: u32 = 0x0367;
+
+    fn size_without_header(&self) -> u32 {
+        self.size() as u32
+    }
+
+    fn write_into_vec(&self, w: &mut Vec<u8>) -> Result<(), std::io::Error> {
+        let size_assert_header_size = w.len();
+        // update_type: LfgUpdateType
+        w.write_all(&(self.update_type.as_int() as u8).to_le_bytes())?;
+
+        // join_status: LfgJoinStatus
+        w.write_all(&(self.join_status.as_int() as u8).to_le_bytes())?;
+
+        match &self.join_status {
+            SMSG_LFG_UPDATE_PLAYER_LfgJoinStatus::NotJoined => {}
+            SMSG_LFG_UPDATE_PLAYER_LfgJoinStatus::Joined {
+                achievements,
+                comment,
+                dungeons,
+                no_partial_clear,
+                queued,
+            } => {
+                // queued: u8
+                w.write_all(&queued.to_le_bytes())?;
+
+                // no_partial_clear: u8
+                w.write_all(&no_partial_clear.to_le_bytes())?;
+
+                // achievements: u8
+                w.write_all(&achievements.to_le_bytes())?;
+
+                // amount_of_dungeons: u8
+                w.write_all(&(dungeons.len() as u8).to_le_bytes())?;
+
+                // dungeons: u32[amount_of_dungeons]
+                for i in dungeons.iter() {
+                    w.write_all(&i.to_le_bytes())?;
+                }
+
+                // comment: CString
+                // TODO: Guard against strings that are already null-terminated
+                assert_ne!(comment.as_bytes().iter().rev().next(), Some(&0_u8), "String `comment` must not be null-terminated.");
+                w.write_all(comment.as_bytes())?;
+                // Null terminator
+                w.write_all(&[0])?;
+
+            }
+        }
+
+        assert_eq!(self.size() as usize + size_assert_header_size, w.len(), "Mismatch in pre-calculated size and actual written size. This needs investigation as it will cause problems in the game client when sent");
+        Ok(())
+    }
+    fn read_body(r: &mut &[u8], body_size: u32) -> std::result::Result<Self, crate::errors::ParseError> {
+        if !(2..=1286).contains(&body_size) {
+            return Err(crate::errors::ParseError::InvalidSize { opcode: 0x0367, size: body_size as u32 });
+        }
+
+        // update_type: LfgUpdateType
+        let update_type: LfgUpdateType = crate::util::read_u8_le(r)?.try_into()?;
+
+        // join_status: LfgJoinStatus
+        let join_status: LfgJoinStatus = crate::util::read_u8_le(r)?.try_into()?;
+
+        let join_status_if = match join_status {
+            LfgJoinStatus::NotJoined => SMSG_LFG_UPDATE_PLAYER_LfgJoinStatus::NotJoined,
+            LfgJoinStatus::Joined => {
+                // queued: u8
+                let queued = crate::util::read_u8_le(r)?;
+
+                // no_partial_clear: u8
+                let no_partial_clear = crate::util::read_u8_le(r)?;
+
+                // achievements: u8
+                let achievements = crate::util::read_u8_le(r)?;
+
+                // amount_of_dungeons: u8
+                let amount_of_dungeons = crate::util::read_u8_le(r)?;
+
+                // dungeons: u32[amount_of_dungeons]
+                let mut dungeons = Vec::with_capacity(amount_of_dungeons as usize);
+                for i in 0..amount_of_dungeons {
+                    dungeons.push(crate::util::read_u32_le(r)?);
+                }
+
+                // comment: CString
+                let comment = crate::util::read_c_string_to_vec(r)?;
+                let comment = String::from_utf8(comment)?;
+
+                SMSG_LFG_UPDATE_PLAYER_LfgJoinStatus::Joined {
+                    achievements,
+                    comment,
+                    dungeons,
+                    no_partial_clear,
+                    queued,
+                }
+            }
+        };
+
+        Ok(Self {
+            update_type,
+            join_status: join_status_if,
+        })
+    }
+
+}
+#[cfg(feature = "wrath")]
+impl crate::world::wrath::ServerMessage for SMSG_LFG_UPDATE_PLAYER {}
+
+impl SMSG_LFG_UPDATE_PLAYER {
+    pub(crate) fn size(&self) -> usize {
+        1 // update_type: LfgUpdateType
+        + self.join_status.size() // join_status: SMSG_LFG_UPDATE_PLAYER_LfgJoinStatus
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum SMSG_LFG_UPDATE_PLAYER_LfgJoinStatus {
+    NotJoined,
+    Joined {
+        achievements: u8,
+        comment: String,
+        dungeons: Vec<u32>,
+        no_partial_clear: u8,
+        queued: u8,
+    },
+}
+
+impl Default for SMSG_LFG_UPDATE_PLAYER_LfgJoinStatus {
+    fn default() -> Self {
+        // First enumerator without any fields
+        Self::NotJoined
+    }
+}
+
+impl SMSG_LFG_UPDATE_PLAYER_LfgJoinStatus {
+    pub(crate) const fn as_int(&self) -> u8 {
+        match self {
+            Self::NotJoined => 0,
+            Self::Joined { .. } => 1,
+        }
+    }
+
+}
+
+impl SMSG_LFG_UPDATE_PLAYER_LfgJoinStatus {
+    pub(crate) fn size(&self) -> usize {
+        match self {
+            Self::NotJoined => {
+                1
+            }
+            Self::Joined {
+                achievements,
+                comment,
+                dungeons,
+                no_partial_clear,
+                queued,
+            } => {
+                1
+                + 1 // achievements: u8
+                + 1 // amount_of_dungeons: u8
+                + comment.len() + 1 // comment: CString
+                + dungeons.len() * core::mem::size_of::<u32>() // dungeons: u32[amount_of_dungeons]
+                + 1 // no_partial_clear: u8
+                + 1 // queued: u8
+            }
+        }
+    }
+}
+
