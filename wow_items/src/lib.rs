bc4bbf5a4a571f4f153a08e29add1b2a5ae7ce59
Gtker <git@gtker.com>
2023-02-15 18:27:29 +0100

    item/spell: Remove macro for all_* and lookup_*

--- wow_items/src/lib.rs
+++ wow_items/src/lib.rs
@@ -4,18 +4,3 @@ pub mod tbc;
 pub mod vanilla;
 #[cfg(feature = "wrath")]
 pub mod wrath;
-
-#[cfg(any(feature = "vanilla", feature = "tbc", feature = "wrath"))]
-macro_rules! find_and_data {
-    ($data:expr) => {
-        pub fn lookup_item(id: u32) -> Option<&'static Item> {
-            all_items().iter().find(|a| a.entry() == id)
-        }
-
-        pub const fn all_items() -> &'static [Item] {
-            $data
-        }
-    };
-}
-#[cfg(any(feature = "vanilla", feature = "tbc", feature = "wrath"))]
-pub(crate) use find_and_data;
--- wow_spells/src/lib.rs
+++ wow_spells/src/lib.rs
@@ -4,18 +4,3 @@ pub mod tbc;
 pub mod vanilla;
 #[cfg(feature = "wrath")]
 pub mod wrath;
-
-#[cfg(any(feature = "vanilla", feature = "tbc", feature = "wrath"))]
-macro_rules! find_and_data {
-    ($data:expr) => {
-        pub fn lookup_spell(id: u32) -> Option<&'static Spell> {
-            all_spells().iter().find(|a| a.entry() == id)
-        }
-
-        pub const fn all_spells() -> &'static [Spell] {
-            $data
-        }
-    };
-}
-#[cfg(any(feature = "vanilla", feature = "tbc", feature = "wrath"))]
-pub(crate) use find_and_data;
