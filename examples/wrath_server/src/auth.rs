ef1fd38e5920c5d2cbbf1d26efb9ca26f4e0b349
Gtker <git@gtker.com>
2023-05-20 23:07:16 +0200

    examples: Remove auth server support for other versions

--- examples/wrath_server/src/auth.rs
+++ examples/wrath_server/src/auth.rs
@@ -40,18 +40,18 @@ async fn handle(mut stream: TcpStream, users: Arc<Mutex<HashMap<String, SrpServe
         }
     };
 
+    // Wrath can only use protocol version eight
     match opcode {
-        InitialMessage::Logon(l) => match l.protocol_version {
-            ProtocolVersion::Two => login_version_2(stream, l, users).await,
-            ProtocolVersion::Three => login_version_3(stream, l, users).await,
-            ProtocolVersion::Eight => login_version_8(stream, l, users).await,
-            _ => {}
-        },
-        InitialMessage::Reconnect(r) => match r.protocol_version {
-            ProtocolVersion::Two => reconnect_version_2(stream, r, users).await,
-            ProtocolVersion::Eight => reconnect_version_8(stream, r, users).await,
-            _ => {}
-        },
+        InitialMessage::Logon(l) => {
+            if let ProtocolVersion::Eight = l.protocol_version {
+                login_version_8(stream, l, users).await
+            }
+        }
+        InitialMessage::Reconnect(r) => {
+            if let ProtocolVersion::Eight = r.protocol_version {
+                reconnect_version_8(stream, r, users).await
+            }
+        }
     }
 }
 
@@ -113,175 +113,12 @@ async fn reconnect_version_8(
     print_version_8_realm_list(stream).await;
 }
 
-async fn reconnect_version_2(
-    mut stream: TcpStream,
-    r: CMD_AUTH_RECONNECT_CHALLENGE_Client,
-    users: Arc<Mutex<HashMap<String, SrpServer>>>,
-) {
-    use wow_login_messages::version_2::*;
-
-    println!("Reconnect version: {}", r.protocol_version);
-
-    let server_reconnect_challenge_data = *users
-        .lock()
-        .unwrap()
-        .get(&r.account_name)
-        .unwrap()
-        .reconnect_challenge_data();
-
-    CMD_AUTH_RECONNECT_CHALLENGE_Server {
-        result: CMD_AUTH_RECONNECT_CHALLENGE_Server_LoginResult::Success {
-            challenge_data: server_reconnect_challenge_data,
-            checksum_salt: [0; 16],
-        },
-    }
-    .tokio_write(&mut stream)
-    .await
-    .unwrap();
-
-    let l = tokio_expect_client_message::<CMD_AUTH_RECONNECT_PROOF_Client, _>(&mut stream)
-        .await
-        .unwrap();
-
-    let success = {
-        match users.lock().unwrap().get_mut(&r.account_name) {
-            None => false,
-            Some(server) => server.verify_reconnection_attempt(l.proof_data, l.client_proof),
-        }
-    };
-
-    if !success {
-        CMD_AUTH_RECONNECT_PROOF_Server {
-            result: LoginResult::FailBanned,
-        }
-        .tokio_write(&mut stream)
-        .await
-        .unwrap();
-
-        return;
-    }
-
-    CMD_AUTH_RECONNECT_PROOF_Server {
-        result: LoginResult::Success,
-    }
-    .tokio_write(&mut stream)
-    .await
-    .unwrap();
-
-    print_version_2_3_realm_list(stream).await;
-}
-
-async fn login_version_2(
-    mut stream: TcpStream,
-    l: CMD_AUTH_LOGON_CHALLENGE_Client,
-    users: Arc<Mutex<HashMap<String, SrpServer>>>,
-) {
-    use wow_login_messages::version_2::*;
-
-    println!("Login version: {}", l.protocol_version);
-    let p = get_proof(&l.account_name);
-
-    let username = l.account_name;
-
-    CMD_AUTH_LOGON_CHALLENGE_Server {
-        result: CMD_AUTH_LOGON_CHALLENGE_Server_LoginResult::Success {
-            server_public_key: *p.server_public_key(),
-            generator: vec![GENERATOR],
-            large_safe_prime: LARGE_SAFE_PRIME_LITTLE_ENDIAN.into(),
-            salt: *p.salt(),
-            crc_salt: [0; 16],
-        },
-    }
-    .tokio_write(&mut stream)
-    .await
-    .unwrap();
-    println!("Sent Logon Challenge");
-
-    let l = tokio_expect_client_message::<CMD_AUTH_LOGON_PROOF_Client, _>(&mut stream)
-        .await
-        .unwrap();
-
-    let (p, proof) = p
-        .into_server(
-            PublicKey::from_le_bytes(l.client_public_key).unwrap(),
-            l.client_proof,
-        )
-        .unwrap();
-
-    CMD_AUTH_LOGON_PROOF_Server {
-        result: CMD_AUTH_LOGON_PROOF_Server_LoginResult::Success {
-            server_proof: proof,
-            hardware_survey_id: 0,
-        },
-    }
-    .tokio_write(&mut stream)
-    .await
-    .unwrap();
-    println!("Sent Logon Proof");
-
-    users.lock().unwrap().insert(username, p);
-
-    print_version_2_3_realm_list(stream).await;
-}
-
 fn get_proof(username: &str) -> SrpProof {
     let username = NormalizedString::new(username.to_string()).unwrap();
     let password = NormalizedString::new(username.to_string()).unwrap();
     SrpVerifier::from_username_and_password(username, password).into_proof()
 }
 
-async fn login_version_3(
-    mut stream: TcpStream,
-    l: CMD_AUTH_LOGON_CHALLENGE_Client,
-    users: Arc<Mutex<HashMap<String, SrpServer>>>,
-) {
-    use wow_login_messages::version_3::*;
-
-    println!("Login version: {}", l.protocol_version);
-    let p = get_proof(&l.account_name);
-    let username = l.account_name;
-
-    CMD_AUTH_LOGON_CHALLENGE_Server {
-        result: CMD_AUTH_LOGON_CHALLENGE_Server_LoginResult::Success {
-            server_public_key: *p.server_public_key(),
-            generator: vec![GENERATOR],
-            large_safe_prime: LARGE_SAFE_PRIME_LITTLE_ENDIAN.into(),
-            salt: *p.salt(),
-            crc_salt: [0; 16],
-            security_flag: CMD_AUTH_LOGON_CHALLENGE_Server_SecurityFlag::None,
-        },
-    }
-    .tokio_write(&mut stream)
-    .await
-    .unwrap();
-    println!("Sent Logon Challenge");
-
-    let l = tokio_expect_client_message::<CMD_AUTH_LOGON_PROOF_Client, _>(&mut stream)
-        .await
-        .unwrap();
-
-    let (p, proof) = p
-        .into_server(
-            PublicKey::from_le_bytes(l.client_public_key).unwrap(),
-            l.client_proof,
-        )
-        .unwrap();
-
-    CMD_AUTH_LOGON_PROOF_Server {
-        result: CMD_AUTH_LOGON_PROOF_Server_LoginResult::Success {
-            server_proof: proof,
-            hardware_survey_id: 0,
-        },
-    }
-    .tokio_write(&mut stream)
-    .await
-    .unwrap();
-    println!("Sent Logon Proof");
-
-    users.lock().unwrap().insert(username.to_string(), p);
-
-    print_version_2_3_realm_list(stream).await;
-}
 async fn login_version_8(
     mut stream: TcpStream,
     l: CMD_AUTH_LOGON_CHALLENGE_Client,
@@ -335,29 +172,6 @@ async fn login_version_8(
     users.lock().unwrap().insert(username.to_string(), p);
 
     print_version_8_realm_list(stream).await;
-}
-
-async fn print_version_2_3_realm_list(mut stream: TcpStream) {
-    use wow_login_messages::version_2::*;
-
-    while (tokio_expect_client_message::<CMD_REALM_LIST_Client, _>(&mut stream).await).is_ok() {
-        CMD_REALM_LIST_Server {
-            realms: vec![Realm {
-                realm_type: RealmType::PlayerVsEnvironment,
-                flag: RealmFlag::empty(),
-                name: "Tester".to_string(),
-                address: "127.0.0.1:8085".to_string(),
-                population: Default::default(),
-                number_of_characters_on_realm: 0,
-                category: Default::default(),
-                realm_id: 0,
-            }],
-        }
-        .tokio_write(&mut stream)
-        .await
-        .unwrap();
-        println!("Sent Version 2/3 Realm List");
-    }
 }
 
 async fn print_version_8_realm_list(mut stream: TcpStream) {
