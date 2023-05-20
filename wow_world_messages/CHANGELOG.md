3ba8d9c8928b90d8498f116e35409f6390dd2625
Gtker <git@gtker.com>
2023-05-20 17:29:23 +0200

    world: Add CHANGELOG, README and release.toml

--- wow_world_messages/CHANGELOG.md
+++ wow_world_messages/CHANGELOG.md
@@ -5,22 +5,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+<!-- next-header -->
+## [Unreleased] - ReleaseDate
+
+
+## [0.1.0] - 2023-05-20
 
 ### Added
 
-### Changed
+* First release.
 
-### Deprecated
-
-### Removed
-
-### Fixed
-
-### Security
-
-## 0.1.0
-
-### Added
-
-* Initial release. This is very WIP and things are not guaranteed to be documented or actually work correctly.
+<!-- next-url -->
+[Unreleased]: https://github.com/gtker/wow_messages/compare/wow_world_messages-v0.1.1...HEAD
+[0.1.0]: https://github.com/gtker/wow_messages/releases/tag/wow_world_messages-v0.1.0
--- wow_world_messages/src/lib.rs
+++ wow_world_messages/src/lib.rs
@@ -29,23 +29,6 @@
 //!  * [`vMaNGOS` (C++)](https://github.com/vmangos/core)
 //!    and `MaNGOS` derivatives in general have a relatively complete list of messages for popular versions,
 //!    but they are not available as a library.
-//!
-//!  ## License
-//!
-//!  Licensed under either of
-//!
-//!  * Apache License, Version 2.0
-//!    ([LICENSE-APACHE](LICENSE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0>)
-//!  * MIT license
-//!    ([LICENSE-MIT](LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
-//!
-//!  at your option.
-//!
-//!  ## Contribution
-//!
-//!  Unless you explicitly state otherwise, any contribution intentionally submitted
-//!  for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
-//!  dual licensed as above, without any additional terms or conditions.
 #![forbid(unsafe_code)]
 #![warn(
     clippy::complexity,
