6e3aa8f0661f47bc211eb3a8433d97ca854f8b1f
Gtker <git@gtker.com>
2024-02-13 19:43:46 +0100

    base: Fix clippy lint for DateTime::leap_year

--- wow_world_base/src/manual/shared/datetime_vanilla_tbc_wrath.rs
+++ wow_world_base/src/manual/shared/datetime_vanilla_tbc_wrath.rs
@@ -350,10 +350,8 @@ const fn leap_year(years_after_2000: u32) -> bool {
     let year = years_after_2000 + 2000;
     if year % 4 == 0 && year % 100 != 0 {
         true
-    } else if year % 400 == 0 {
-        true
     } else {
-        false
+        year % 400 == 0
     }
 }
 
