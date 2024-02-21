b93d8b6079c481fd34d40a5dd3cbfb7bc9ed8fad
Gtker <git@gtker.com>
2024-02-21 17:15:08 +0100

    Add clippy with all features to pre-release.sh

--- pre-release.sh
+++ pre-release.sh
@@ -7,5 +7,9 @@ export CARGO_INCREMENTAL=0
 cargo install cargo-hack --locked
 
 cargo test --all-features -p $1
+
+# Try to catch clippy lints early
+cargo clippy --all-features -p $1
+
 cargo hack clippy --feature-powerset -p $1
 
