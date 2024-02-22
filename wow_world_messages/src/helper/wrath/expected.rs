0812facb83ce771ddabbb30d048842a123450579
Automated Gtker <autogit@gtker.com>
2024-02-22 01:45:04 +0100

    Auto update wow_db_sqlite version

--- wow_world_messages/src/helper/wrath/expected.rs
+++ wow_world_messages/src/helper/wrath/expected.rs
@@ -13,10 +13,10 @@ pub fn expect_server_message<M: crate::wrath::ServerMessage, R: std::io::Read>(
         crate::util::ServerHeader::from_array(buf)
     };
 
-    let mut buf = vec![0_u8; d.size.saturating_sub(2) as usize];
+    let mut buf = vec![0_u8; (d.size.saturating_sub(2)) as usize];
     r.read_exact(&mut buf)?;
 
-    read_server_body(&mut buf.as_slice(), d.size, d.opcode.into())
+    read_server_body(&mut buf.as_slice(), d.size.into(), d.opcode.into())
 }
 
 #[cfg(feature = "sync")]
@@ -27,10 +27,10 @@ pub fn expect_client_message<M: crate::wrath::ClientMessage, R: std::io::Read>(
     r.read_exact(&mut header)?;
     let d = crate::util::ClientHeader::from_array(header);
 
-    let mut buf = vec![0_u8; d.size.saturating_sub(4) as usize];
+    let mut buf = vec![0_u8; (d.size.saturating_sub(4)) as usize];
     r.read_exact(&mut buf)?;
 
-    read_client_body(&mut buf.as_slice(), d.size, d.opcode)
+    read_client_body(&mut buf.as_slice(), d.size.into(), d.opcode.into())
 }
 
 #[cfg(all(feature = "sync", feature = "encryption"))]
@@ -49,10 +49,10 @@ pub fn expect_server_message_encryption<M: crate::wrath::ServerMessage, R: std::
         }
     };
 
-    let mut buf = vec![0_u8; d.size.saturating_sub(2) as usize];
+    let mut buf = vec![0_u8; (d.size.saturating_sub(2)) as usize];
     r.read_exact(&mut buf)?;
 
-    read_server_body(&mut buf.as_slice(), d.size, d.opcode.into())
+    read_server_body(&mut buf.as_slice(), d.size.into(), d.opcode.into())
 }
 
 #[cfg(all(feature = "sync", feature = "encryption"))]
@@ -64,17 +64,14 @@ pub fn expect_client_message_encryption<M: crate::wrath::ClientMessage, R: std::
     r.read_exact(&mut header)?;
     let d = d.decrypt_client_header(header);
 
-    let mut buf = vec![0_u8; d.size.saturating_sub(4) as usize];
+    let mut buf = vec![0_u8; (d.size.saturating_sub(4)) as usize];
     r.read_exact(&mut buf)?;
 
-    read_client_body(&mut buf.as_slice(), d.size, d.opcode)
+    read_client_body(&mut buf.as_slice(), d.size.into(), d.opcode.into())
 }
 
 #[cfg(feature = "tokio")]
-pub async fn tokio_expect_server_message<
-    M: crate::wrath::ServerMessage,
-    R: tokio::io::AsyncReadExt + Unpin + Send,
->(
+pub async fn tokio_expect_server_message<M: crate::wrath::ServerMessage, R: tokio::io::AsyncReadExt + Unpin + Send>(
     r: &mut R,
 ) -> Result<M, crate::errors::ExpectedOpcodeError> {
     let mut buf = [0_u8; 4];
@@ -88,34 +85,28 @@ pub async fn tokio_expect_server_message<
         crate::util::ServerHeader::from_array(buf)
     };
 
-    let mut buf = vec![0_u8; d.size.saturating_sub(2) as usize];
+    let mut buf = vec![0_u8; (d.size.saturating_sub(2)) as usize];
     r.read_exact(&mut buf).await?;
 
-    read_server_body(&mut buf.as_slice(), d.size, d.opcode.into())
+    read_server_body(&mut buf.as_slice(), d.size.into(), d.opcode.into())
 }
 
 #[cfg(feature = "tokio")]
-pub async fn tokio_expect_client_message<
-    M: crate::wrath::ClientMessage,
-    R: tokio::io::AsyncReadExt + Unpin + Send,
->(
+pub async fn tokio_expect_client_message<M: crate::wrath::ClientMessage, R: tokio::io::AsyncReadExt + Unpin + Send>(
     r: &mut R,
 ) -> Result<M, crate::errors::ExpectedOpcodeError> {
     let mut header = [0_u8; 6];
     r.read_exact(&mut header).await?;
     let d = crate::util::ClientHeader::from_array(header);
 
-    let mut buf = vec![0_u8; d.size.saturating_sub(4) as usize];
+    let mut buf = vec![0_u8; (d.size.saturating_sub(4)) as usize];
     r.read_exact(&mut buf).await?;
 
-    read_client_body(&mut buf.as_slice(), d.size, d.opcode)
+    read_client_body(&mut buf.as_slice(), d.size.into(), d.opcode.into())
 }
 
 #[cfg(all(feature = "tokio", feature = "encryption"))]
-pub async fn tokio_expect_server_message_encryption<
-    M: crate::wrath::ServerMessage,
-    R: tokio::io::AsyncReadExt + Unpin + Send,
->(
+pub async fn tokio_expect_server_message_encryption<M: crate::wrath::ServerMessage, R: tokio::io::AsyncReadExt + Unpin + Send>(
     r: &mut R,
     d: &mut wow_srp::wrath_header::ClientDecrypterHalf,
 ) -> Result<M, crate::errors::ExpectedOpcodeError> {
@@ -130,17 +121,14 @@ pub async fn tokio_expect_server_message_encryption<
         }
     };
 
-    let mut buf = vec![0_u8; d.size.saturating_sub(2) as usize];
+    let mut buf = vec![0_u8; (d.size.saturating_sub(2)) as usize];
     r.read_exact(&mut buf).await?;
 
-    read_server_body(&mut buf.as_slice(), d.size, d.opcode.into())
+    read_server_body(&mut buf.as_slice(), d.size.into(), d.opcode.into())
 }
 
 #[cfg(all(feature = "tokio", feature = "encryption"))]
-pub async fn tokio_expect_client_message_encryption<
-    M: crate::wrath::ClientMessage,
-    R: tokio::io::AsyncReadExt + Unpin + Send,
->(
+pub async fn tokio_expect_client_message_encryption<M: crate::wrath::ClientMessage, R: tokio::io::AsyncReadExt + Unpin + Send>(
     r: &mut R,
     d: &mut wow_srp::wrath_header::ServerDecrypterHalf,
 ) -> Result<M, crate::errors::ExpectedOpcodeError> {
@@ -148,17 +136,14 @@ pub async fn tokio_expect_client_message_encryption<
     r.read_exact(&mut header).await?;
     let d = d.decrypt_client_header(header);
 
-    let mut buf = vec![0_u8; d.size.saturating_sub(4) as usize];
+    let mut buf = vec![0_u8; (d.size.saturating_sub(4)) as usize];
     r.read_exact(&mut buf).await?;
 
-    read_client_body(&mut buf.as_slice(), d.size, d.opcode)
+    read_client_body(&mut buf.as_slice(), d.size.into(), d.opcode.into())
 }
 
 #[cfg(feature = "async-std")]
-pub async fn astd_expect_server_message<
-    M: crate::wrath::ServerMessage,
-    R: async_std::io::ReadExt + Unpin + Send,
->(
+pub async fn astd_expect_server_message<M: crate::wrath::ServerMessage, R: async_std::io::ReadExt + Unpin + Send>(
     r: &mut R,
 ) -> Result<M, crate::errors::ExpectedOpcodeError> {
     let mut buf = [0_u8; 4];
@@ -172,34 +157,28 @@ pub async fn astd_expect_server_message<
         crate::util::ServerHeader::from_array(buf)
     };
 
-    let mut buf = vec![0_u8; d.size.saturating_sub(2) as usize];
+    let mut buf = vec![0_u8; (d.size.saturating_sub(2)) as usize];
     r.read_exact(&mut buf).await?;
 
-    read_server_body(&mut buf.as_slice(), d.size, d.opcode.into())
+    read_server_body(&mut buf.as_slice(), d.size.into(), d.opcode.into())
 }
 
 #[cfg(feature = "async-std")]
-pub async fn astd_expect_client_message<
-    M: crate::wrath::ClientMessage,
-    R: async_std::io::ReadExt + Unpin + Send,
->(
+pub async fn astd_expect_client_message<M: crate::wrath::ClientMessage, R: async_std::io::ReadExt + Unpin + Send>(
     r: &mut R,
 ) -> Result<M, crate::errors::ExpectedOpcodeError> {
     let mut header = [0_u8; 6];
     r.read_exact(&mut header).await?;
     let d = crate::util::ClientHeader::from_array(header);
 
-    let mut buf = vec![0_u8; d.size.saturating_sub(4) as usize];
+    let mut buf = vec![0_u8; (d.size.saturating_sub(4)) as usize];
     r.read_exact(&mut buf).await?;
 
-    read_client_body(&mut buf.as_slice(), d.size, d.opcode)
+    read_client_body(&mut buf.as_slice(), d.size.into(), d.opcode.into())
 }
 
 #[cfg(all(feature = "async-std", feature = "encryption"))]
-pub async fn astd_expect_server_message_encryption<
-    M: crate::wrath::ServerMessage,
-    R: async_std::io::ReadExt + Unpin + Send,
->(
+pub async fn astd_expect_server_message_encryption<M: crate::wrath::ServerMessage, R: async_std::io::ReadExt + Unpin + Send>(
     r: &mut R,
     d: &mut wow_srp::wrath_header::ClientDecrypterHalf,
 ) -> Result<M, crate::errors::ExpectedOpcodeError> {
@@ -214,17 +193,14 @@ pub async fn astd_expect_server_message_encryption<
         }
     };
 
-    let mut buf = vec![0_u8; d.size.saturating_sub(2) as usize];
+    let mut buf = vec![0_u8; (d.size.saturating_sub(2)) as usize];
     r.read_exact(&mut buf).await?;
 
-    read_server_body(&mut buf.as_slice(), d.size, d.opcode.into())
+    read_server_body(&mut buf.as_slice(), d.size.into(), d.opcode.into())
 }
 
 #[cfg(all(feature = "async-std", feature = "encryption"))]
-pub async fn astd_expect_client_message_encryption<
-    M: crate::wrath::ClientMessage,
-    R: async_std::io::ReadExt + Unpin + Send,
->(
+pub async fn astd_expect_client_message_encryption<M: crate::wrath::ClientMessage, R: async_std::io::ReadExt + Unpin + Send>(
     r: &mut R,
     d: &mut wow_srp::wrath_header::ServerDecrypterHalf,
 ) -> Result<M, crate::errors::ExpectedOpcodeError> {
@@ -232,10 +208,10 @@ pub async fn astd_expect_client_message_encryption<
     r.read_exact(&mut header).await?;
     let d = d.decrypt_client_header(header);
 
-    let mut buf = vec![0_u8; d.size.saturating_sub(4) as usize];
+    let mut buf = vec![0_u8; (d.size.saturating_sub(4)) as usize];
     r.read_exact(&mut buf).await?;
 
-    read_client_body(&mut buf.as_slice(), d.size, d.opcode)
+    read_client_body(&mut buf.as_slice(), d.size.into(), d.opcode.into())
 }
 
 #[cfg(any(feature = "sync", feature = "tokio", feature = "async-std"))]
@@ -246,7 +222,10 @@ fn read_server_body<M: crate::wrath::ServerMessage>(
 ) -> Result<M, crate::errors::ExpectedOpcodeError> {
     // Unable to match on associated const M::OPCODE, so we do if
     if opcode == M::OPCODE {
-        let m = M::read_body::<crate::traits::private::Internal>(buf, size.saturating_sub(2));
+        let m = M::read_body::<crate::traits::private::Internal>(
+            buf,
+            size.saturating_sub(2) as u32,
+        );
         match m {
             Ok(m) => Ok(m),
             Err(e) => Err(e.into()),
@@ -255,11 +234,10 @@ fn read_server_body<M: crate::wrath::ServerMessage>(
         Err(crate::errors::ExpectedOpcodeError::Opcode {
             opcode,
             name: crate::wrath::opcode_to_name(opcode),
-            size,
+            size: size.into(),
         })
     }
 }
-
 #[cfg(any(feature = "sync", feature = "tokio", feature = "async-std"))]
 fn read_client_body<M: crate::wrath::ClientMessage>(
     buf: &mut &[u8],
@@ -268,8 +246,10 @@ fn read_client_body<M: crate::wrath::ClientMessage>(
 ) -> Result<M, crate::errors::ExpectedOpcodeError> {
     // Unable to match on associated const M::OPCODE, so we do if
     if opcode == M::OPCODE {
-        let m =
-            M::read_body::<crate::traits::private::Internal>(buf, size.saturating_sub(4) as u32);
+        let m = M::read_body::<crate::traits::private::Internal>(
+            buf,
+            size.saturating_sub(4) as u32,
+        );
         match m {
             Ok(m) => Ok(m),
             Err(e) => Err(e.into()),
